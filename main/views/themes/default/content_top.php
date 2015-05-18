daab7e5c407c7cbdbe4e19c20e37c50833026ed7
sampoyigi <sam@sampoyigi.com>
2015-05-18 22:42:43 +0100

    check changelog.txt

---
+++ main/views/themes/default/content_top.php
@@ -0,0 +1,7 @@
+<?php if (!empty($top_modules)) { ?>
+    <div id="module-top" class="top-section">
+        <?php foreach ($top_modules as $module) { ?>
+            <?php echo $module; ?>
+        <?php } ?>
+    </div>
+<?php } ?>
