ceaef6a511cf33d0212cf2969968a5addfce3233
sampoyigi <sam@sampoyigi.com>
2015-06-06 02:53:59 +0100

    minor changes

--- admin/views/themes/tastyigniter-blue/activities_latest.php
+++ admin/views/themes/tastyigniter-blue/activities_latest.php
@@ -4,7 +4,7 @@
             <li class="<?php echo $activity['state']; ?>">
                 <div class="clearfix">
                     <span class="activity-body"><i class="<?php echo $activity['icon']; ?> fa-fw"></i> <?php echo $activity['message']; ?></span>
-                    <span class="text-muted small"><?php echo $activity['time']; ?></span>
+                    <span class="activity-time text-muted text-right small"><?php echo $activity['time']; ?></span>
                 </div>
             </li>
             <li class="divider"></li>
