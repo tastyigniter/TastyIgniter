6b35ccedb372567a01dcaa58f04e06a10841a481
sampoyigi <sam@sampoyigi.com>
2015-06-25 17:25:34 +0100

    minor fix

---
+++ system/tastyigniter/language/english/tastyigniter_lang.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+/* End of file tastyigniter_lang.php */
+/* Location: ./system/tastyigniter/language/english/tastyigniter_lang.php */
