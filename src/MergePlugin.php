391e19a58a0cab451beea98772760166e601a90d
Bryan Davis <bd808@wikimedia.org>
2015-06-26 12:13:54 -0600

    Touch up credits information
    * Add Bryan Davis as author in composer.json
    * Bump copyright dates to current year

--- src/MergePlugin.php
+++ src/MergePlugin.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the Composer Merge plugin.
  *
- * Copyright (C) 2014 Bryan Davis, Wikimedia Foundation, and contributors
+ * Copyright (C) 2015 Bryan Davis, Wikimedia Foundation, and contributors
  *
  * This software may be modified and distributed under the terms of the MIT
  * license. See the LICENSE file for details.
