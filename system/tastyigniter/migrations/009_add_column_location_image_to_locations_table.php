c5f43e5400b8cf3a235f000e91e141a6be232425
sampoyigi <sam@sampoyigi.com>
2015-07-12 09:19:07 +0100

    fix reservation_stay_time doesn't exist in the database

--- system/tastyigniter/migrations/009_add_column_location_image_to_locations_table.php
+++ system/tastyigniter/migrations/009_add_column_location_image_to_locations_table.php
@@ -6,12 +6,14 @@ class Migration_add_column_location_image_to_locations_table extends CI_Migratio
         $this->dbforge->add_column('locations', array('location_image VARCHAR(255) NOT NULL'));
 
         $this->db->query("ALTER TABLE ".$this->db->dbprefix('locations')." CHANGE `reservation_interval` `reservation_time_interval` INT(11) NOT NULL;");
+        $this->db->query("ALTER TABLE ".$this->db->dbprefix('locations')." CHANGE `reservation_turn` `reservation_stay_time` INT(11) NOT NULL;");
     }
 
     public function down() {
         $this->dbforge->drop_column('locations', 'location_image');
 
         $this->db->query("ALTER TABLE ".$this->db->dbprefix('locations')." CHANGE `reservation_time_interval` `reservation_interval` INT(11) NOT NULL;");
+        $this->db->query("ALTER TABLE ".$this->db->dbprefix('locations')." CHANGE `reservation_stay_time` `reservation_turn` INT(11) NOT NULL;");
     }
 }
 
