00708e2711dd73e6a0fcb7d64dd42a0352e49e8f
sampoyigi <sam@sampoyigi.com>
2015-08-01 16:35:08 +0100

    minor changes

--- admin/views/themes/tastyigniter-blue/dashboard.php
+++ admin/views/themes/tastyigniter-blue/dashboard.php
@@ -81,24 +81,24 @@
                             </div>
 							<span class="text-center"><?php echo lang('text_progress_summary'); ?></span><br />
 							<div class="list-group check-list-group">
-								<a href="#" class="list-group-item">
-                                    <span class="check-icon pull-left"><i class="fa fa-check"></i></span>
-                                    <span class="check-info"><?php echo lang('text_initial_progress'); ?></span>
+                                <a class="list-group-item">
+                                    <span class="check-icon pull-left"><i class="fa fa-check-square-o"></i></span>
+                                    <span class="check-info"><s><?php echo lang('text_initial_progress'); ?><s></span>
                                 </a>
-								<a href="#" class="list-group-item">
-                                    <span class="check-icon pull-left"><i class="fa fa-check"></i></span>
+                                <a href="<?php echo site_url('settings'); ?>" class="list-group-item">
+                                    <span class="check-icon pull-left"><i class="fa fa-square-o"></i></span>
                                     <span class="check-info"><?php echo lang('text_settings_progress'); ?></span>
                                 </a>
-								<a href="#" class="list-group-item">
-                                    <span class="check-icon pull-left"><i class="fa fa-check"></i></span>
+                                <a href="<?php echo site_url('menus'); ?>" class="list-group-item">
+                                    <span class="check-icon pull-left"><i class="fa fa-square-o"></i></span>
                                     <span class="check-info"><?php echo lang('text_menus_progress'); ?></span>
                                 </a>
-								<a href="#" class="list-group-item">
-                                    <span class="check-icon pull-left"><i class="fa fa-check"></i></span>
+                                <a href="<?php echo site_url('themes'); ?>" class="list-group-item">
+                                    <span class="check-icon pull-left"><i class="fa fa-square-o"></i></span>
                                     <span class="check-info"><?php echo lang('text_design_progress'); ?></span>
                                 </a>
-								<a href="#" class="list-group-item">
-                                    <span class="check-icon pull-left"><i class="fa fa-check"></i></span>
+                                <a href="<?php echo site_url('mail_templates'); ?>" class="list-group-item">
+                                    <span class="check-icon pull-left"><i class="fa fa-square-o"></i></span>
                                     <span class="check-info"><?php echo lang('text_email_progress'); ?></span>
                                 </a>
 							</div>
