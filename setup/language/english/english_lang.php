2aefdc0912f3b09238503a5fa43fafca95d3ffce
sampoyigi <sam@sampoyigi.com>
2015-08-01 12:20:34 +0100

    remove Language file contains no data in log file

--- setup/language/english/english_lang.php
+++ setup/language/english/english_lang.php
@@ -1,5 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+$lang['text_min'] 		                            = 'min';
 
 /* End of file english_lang.php */
 /* Location: ./setup/language/english/english_lang.php */
