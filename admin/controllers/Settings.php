0b0a530e6b09b0961808952ec82165b77023a38b
sampoyigi <sam@sampoyigi.com>
2015-08-01 14:51:18 +0100

    fixed duplicate locations on settings update

--- admin/controllers/Settings.php
+++ admin/controllers/Settings.php
@@ -255,9 +255,11 @@ class Settings extends Admin_Controller {
 			);
 
 
+			$this->load->model('Locations_model');
 			if (!empty($update['main_address']) AND is_array($update['main_address'])) {
-				$this->load->model('Locations_model');
-				$this->Locations_model->updateDefault($update['main_address']);
+				if ($this->Locations_model->updateDefault($update['main_address'])) {
+					unset($update['main_address']);
+				}
 			}
 
 			if ($this->Settings_model->updateSettings('config', $update)) {
