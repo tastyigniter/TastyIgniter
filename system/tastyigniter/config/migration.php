4d149be7f281fabaebc1753eab568661709f53b3
sampoyigi <sam@sampoyigi.com>
2015-08-23 12:27:02 +0100

    [updated] migration version number to latest

--- system/tastyigniter/config/migration.php
+++ system/tastyigniter/config/migration.php
@@ -68,7 +68,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 9;
+$config['migration_version'] = 10;
 
 /*
 |--------------------------------------------------------------------------
