70078dc5dc2d891a57f1950049b819e4bdb426b1
sampoyigi <sam@sampoyigi.com>
2015-08-24 13:54:05 +0100

    [fixed] avoid duplicate coupon codes when coupon is created

--- system/tastyigniter/migrations/011_add_column_order_restriction_to_coupons_table.php
+++ system/tastyigniter/migrations/011_add_column_order_restriction_to_coupons_table.php
@@ -4,10 +4,14 @@ class Migration_add_column_order_restriction_to_coupons_table extends CI_Migrati
 
     public function up() {
         $this->dbforge->add_column('coupons', array('order_restriction TINYINT NOT NULL'));
+
+        $this->db->query("ALTER TABLE ".$this->db->dbprefix('coupons')." ADD UNIQUE INDEX (`code`);");
     }
 
     public function down() {
         $this->dbforge->drop_column('coupons', 'order_restriction');
+
+        $this->db->query("ALTER TABLE ".$this->db->dbprefix('coupons')." DROP INDEX `code`;");
     }
 }
 
