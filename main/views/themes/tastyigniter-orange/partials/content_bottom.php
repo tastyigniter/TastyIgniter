1517fb49fe37cc3991c761f0aa24e878150c2908
sampoyigi <sam@sampoyigi.com>
2015-08-28 00:02:45 +0100

    [deleted] DEPRECATED content_ partial files.

--- main/views/themes/tastyigniter-orange/partials/content_bottom.php
+++
@@ -1,7 +0,0 @@
-<?php if (!empty($bottom_modules)) { ?>
-	<div id="module-bottom" class="bottom-section {class}">
-		<?php foreach ($bottom_modules as $module) { ?>
-			<?php echo $module; ?>
-		<?php } ?>
-	</div>
-<?php } ?>
