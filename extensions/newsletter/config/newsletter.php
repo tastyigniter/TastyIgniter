f3e3182c5e85e3b5abf8752562ff6e1c00932c87
sampoyigi <sam@sampoyigi.com>
2015-08-28 11:53:56 +0100

    [added] newsletter module to be displayed in partials

---
+++ extensions/newsletter/config/newsletter.php
@@ -0,0 +1,21 @@
+<?php defined('EXTPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Extension Meta
+|--------------------------------------------------------------------------
+|
+| The Extension Meta Data (Required)
+| 'author'      => The name of the extension author. More than one author may be listed, separated by comma.
+| 'name'        => The name of your extension
+| 'type'        => The type of your extension, could be module, payment or widget
+| 'description' => A short description of the extension. Keep this description to fewer than 128 characters.
+|
+*/
+$config['extension_meta'] = array(
+    'author'		=> 'SamPoyigi',
+    'name'			=> 'newsletter',
+    'type'		    => 'module',
+    'description'   => 'This extension will allow you to place a newsletter subscribe module around your website.',
+    'settings'      => TRUE,
+);
