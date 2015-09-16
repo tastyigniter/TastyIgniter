e8d33e38443de843f6250822ccf56d0c0979e5d0
sampoyigi <sam@sampoyigi.com>
2015-09-16 14:51:05 +0100

    [fixed] admin login redirecting to logout page after successful login

--- system/tastyigniter/core/Admin_Controller.php
+++ system/tastyigniter/core/Admin_Controller.php
@@ -18,7 +18,7 @@ class Admin_Controller extends Base_Controller {
 
         $this->load->library('user');
 
-        if (!$this->user->isLogged() AND $this->uri->rsegment(1) !== 'login') {
+        if (!$this->user->isLogged() AND $this->uri->rsegment(1) !== 'login' AND $this->uri->rsegment(1) !== 'logout') {
             $this->alert->set('danger', 'You must be logged in to access that page.');
             $this->session->set_tempdata('previous_url', current_url());
             redirect(root_url(ADMINDIR.'/login'));
