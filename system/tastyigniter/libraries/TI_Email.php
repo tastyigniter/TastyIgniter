c874b617ebcb315643856a5ef2333edf2e2d2984
sampoyigi <sam@sampoyigi.com>
2015-10-23 14:51:53 +0100

    Added second parameter to both subject() and message() method of TI_Email library to parse data into mail template

--- system/tastyigniter/libraries/TI_Email.php
+++ system/tastyigniter/libraries/TI_Email.php
@@ -32,9 +32,29 @@ class TI_Email extends CI_Email {
 
     // --------------------------------------------------------------------
 
+    public function subject($subject, $parse_data = NULL) {
+        if (!empty($parse_data)) {
+            $subject = $this->parse_template($subject, $parse_data);
+        }
+
+        return parent::subject($subject);
+    }
+
+    public function message($body, $parse_data = NULL) {
+        if (!empty($parse_data)) {
+            $body = $this->parse_template($body, $parse_data);
+        }
+
+        $body = ($this->mailtype === 'html') ? $this->_build_html_mail($body) : strip_tags($body);
+
+        return parent::message($body);
+    }
+
     public function parse_template($template, $data = array()) {
         if (!is_string($template) OR !is_array($data)) return NULL;
+
         $data['site_name'] = $this->CI->config->item('site_name');
+        $data['signature'] = $this->CI->config->item('site_name');
         $data['site_url'] = root_url();
 
         $this->CI->load->model('Image_tool_model');
@@ -43,12 +63,6 @@ class TI_Email extends CI_Email {
         $this->CI->load->library('parser');
 
         return $this->CI->parser->parse_string($template, $data);
-    }
-
-    public function message($body) {
-        $body = ($this->mailtype === 'html') ? $this->_build_html_mail($body) : strip_tags($body);
-
-        return parent::message($body);
     }
 
     private function _build_html_mail($body = '') {
@@ -62,7 +76,46 @@ class TI_Email extends CI_Email {
 						body {
 							font-family: Arial, Verdana, Helvetica, sans-serif;
 							font-size: 16px;
-						}
+                            padding: 0;
+                            margin: 0;
+                        }
+
+                        html { -webkit-text-size-adjust:none; -ms-text-size-adjust: none;}
+                        @media only screen and (max-device-width: 680px), only screen and (max-width: 680px) {
+                            *[class="table_width_100"] {
+                                width: 96% !important;
+                            }
+                            *[class="border-right_mob"] {
+                                border-right: 1px solid #dddddd;
+                            }
+                            *[class="mob_100"] {
+                                width: 100% !important;
+                            }
+                            *[class="mob_center"] {
+                                text-align: center !important;
+                            }
+                            *[class="mob_center_bl"] {
+                                float: none !important;
+                                display: block !important;
+                                margin: 0px auto;
+                            }
+                            .iage_footer a {
+                                text-decoration: none;
+                                color: #929ca8;
+                            }
+                            img.mob_display_none {
+                                width: 0px !important;
+                                height: 0px !important;
+                                display: none !important;
+                            }
+                            img.mob_width_50 {
+                                width: 40% !important;
+                                height: auto !important;
+                            }
+                        }
+                        .table_width_100 {
+                            width: 680px;
+                        }
 					</style>';
         $build .= '</head>';
         $build .= '<body>';
