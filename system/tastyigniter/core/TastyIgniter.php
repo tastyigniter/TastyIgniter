36aa99df7f826fe64c1e197edb4ef2cbd0877ead
sampoyigi <sam@sampoyigi.com>
2015-10-30 18:18:09 +0000

    Update core version number

--- system/tastyigniter/core/TastyIgniter.php
+++ system/tastyigniter/core/TastyIgniter.php
@@ -7,7 +7,7 @@
  *
  */
 	define('CI_VERSION', '3.0-dev');
-	define('TI_VERSION', 'v1.4.2-beta');
+	define('TI_VERSION', 'v1.4.3');
 
 	// Path to the root folder
 	defined('ROOTPATH') OR define('ROOTPATH', str_replace(SYSDIR.'/', '', BASEPATH));
