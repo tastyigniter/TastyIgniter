f8025721b90c093250b37603d791ae9862324818
sampoyigi <sam@sampoyigi.com>
2015-12-03 19:06:59 +0000

    Open copyright links in blank page

--- system/tastyigniter/language/english/tastyigniter_lang.php
+++ system/tastyigniter/language/english/tastyigniter_lang.php
@@ -1,8 +1,8 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['ti_text_system_name']                    = 'TastyIgniter';
-$lang['ti_text_system_powered']                 = '<a href="http://tastyigniter.com">Powered by TastyIgniter</a>';
-$lang['ti_text_copyright'] 		                = 'Thank you for using <a href="http://tastyigniter.com">TastyIgniter</a>';
+$lang['ti_text_system_powered']                 = '<a target="_blank" href="http://tastyigniter.com">Powered by TastyIgniter</a>';
+$lang['ti_text_copyright'] 		                = 'Thank you for using <a target="_blank" href="http://tastyigniter.com">TastyIgniter</a>';
 $lang['ti_text_version'] 		                = 'Version %s';
 
 /* End of file tastyigniter_lang.php */
