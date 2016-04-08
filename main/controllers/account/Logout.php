663b7ef80fb03fc15711d2892a024b07c5c62206
sampoyigi <sam@sampoyigi.com>
2016-04-08 21:28:41 +0100

    Fixed issue where customer is redirected to home page after login

--- main/controllers/account/Logout.php
+++ main/controllers/account/Logout.php
@@ -24,7 +24,7 @@ class Logout extends Main_Controller {
 			redirect($redirect_url);
 		}
 
-        redirect($this->customer->login_url);
+		redirect('account/login');
 	}
 }
 
--- main/controllers/account/Register.php
+++ main/controllers/account/Register.php
@@ -12,7 +12,12 @@ class Register extends Main_Controller {
 	public function index() {
 		if ($this->input->post() AND $this->_addCustomer() === TRUE) {							// checks if $_POST data is set and if registration validation was successful
 			$this->alert->set('alert', $this->lang->line('alert_account_created'));	// display success message and redirect to account login page
-			redirect($this->customer->login_url);
+
+			if ($redirect_url = $this->input->get('redirect')) {
+				redirect($redirect_url);
+			}
+
+			redirect('account/login');
 		}
 
 		$this->template->setTitle($this->lang->line('text_register_heading'));
