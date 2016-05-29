bbae539796290a2ddf431ba22c2464cb77aa64c1
sampoyigi <sam@sampoyigi.com>
2016-05-29 01:07:31 +0100

    Improved send message to all newsletter subscriber feature to include emails submitted via newsletter extension

--- system/tastyigniter/models/Customers_model.php
+++ system/tastyigniter/models/Customers_model.php
@@ -189,6 +189,12 @@ class Customers_model extends TI_Model {
 		if ($query->num_rows() > 0) {
 			foreach ($query->result_array() as $row)
 				$result[] = ($type === 'email') ? $row['email'] : $row['customer_id'];
+
+			$this->load->model('Extensions_model');
+			$newsletter = $this->Extensions_model->getModule('newsletter');
+			if ($type === 'email' AND !empty($newsletter['ext_data']['subscribe_list'])) {
+				$result = array_merge($result, $newsletter['ext_data']['subscribe_list']);
+			}
 		}
 
 		return $result;
@@ -249,7 +255,7 @@ class Customers_model extends TI_Model {
 
 					$this->load->model('Mail_templates_model');
 					$mail_template = $this->Mail_templates_model->getTemplateData($this->config->item('mail_template_id'),
-					                                                              'password_reset');
+																				  'password_reset');
 
 					$this->sendMail($row['email'], $mail_template, $mail_data);
 
