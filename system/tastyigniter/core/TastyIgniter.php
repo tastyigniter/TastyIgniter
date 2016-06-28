1086c004688252c8be7a386041740468c5a920bf
Sam Poyigi <sampoyigi@users.noreply.github.com>
2016-06-28 13:48:49 +0100

    Update version number (#128)

--- system/tastyigniter/core/TastyIgniter.php
+++ system/tastyigniter/core/TastyIgniter.php
@@ -19,7 +19,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @var	string
  *
  */
-	define('TI_VERSION', '2.1.0');
+	define('TI_VERSION', '2.1.1');
 
 	// Path to the root folder
 	defined('ROOTPATH') OR define('ROOTPATH', str_replace(SYSDIR.'/', '', BASEPATH));
