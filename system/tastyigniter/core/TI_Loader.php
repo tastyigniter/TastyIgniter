4b4eecbd9d9393fbdbc9d90e0eb677461836ca88
sampoyigi <sam@sampoyigi.com>
2016-08-12 14:08:51 +0100

    Moved load_db_config to Config library and new restaurant_url method

--- system/tastyigniter/core/TI_Loader.php
+++ system/tastyigniter/core/TI_Loader.php
@@ -160,50 +160,6 @@ class TI_Loader extends MX_Loader {
             $this->model($autoload['model']);
         }
     }
-
-    // --------------------------------------------------------------------
-
-	/**
-     * Load config from database
-     *
-     * Fetches the config values from the database and adds them to config array
-     *
-     */
-    protected function _load_db_config() {
-        if ($this->_db_config_loaded === TRUE) {
-            return;
-        }
-
-        $this->model('Settings_model');
-
-        // Make sure the database is connected and settings table exists
-        if ($this->Settings_model->table_exists()) {
-
-            $this->Settings_model->query("SET SESSION sql_mode=''");
-
-            if ($settings = $this->Settings_model->getAll()) {
-                foreach ($settings as $setting) {
-                    if ( ! empty($setting['serialized'])) {
-                        $this->config->set_item($setting['item'], unserialize($setting['value']));
-                    } else {
-                        $this->config->set_item($setting['item'], $setting['value']);
-                    }
-                }
-
-                if ($this->config->item('timezone')) {
-                    date_default_timezone_set($this->config->item('timezone'));
-                }
-
-                if ($this->config->item('site_url')) {
-                    $this->config->set_item('base_url', trim($this->config->item('site_url'), '/') . '/' . (APPDIR === MAINDIR) ? '' : APPDIR);
-                }
-
-                $this->_db_config_loaded = TRUE;
-
-                log_message('info', 'Database Config Loaded');
-            }
-        }
-    }
 }
 
 /* End of file TI_Loader.php */
