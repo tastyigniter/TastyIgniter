c14c13e4e336b3e41e72a43bfe9bb09d0d7d028f
sampoyigi <sam@sampoyigi.com>
2016-08-23 23:46:27 +0100

    New array helper with array_get method to get array item using dot notation

---
+++ system/tastyigniter/helpers/TI_array_helper.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * TastyIgniter
+ *
+ * An open source online ordering, reservation and management system for restaurants.
+ *
+ * @package   TastyIgniter
+ * @author    SamPoyigi
+ * @copyright TastyIgniter
+ * @link      http://tastyigniter.com
+ * @license   http://opensource.org/licenses/GPL-3.0 The GNU GENERAL PUBLIC LICENSE
+ * @since     File available since Release 1.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Array helper functions
+ *
+ * @category       Helpers
+ * @package        TastyIgniter\Helpers\TI_array_helper.php
+ * @link           http://docs.tastyigniter.com
+ */
+
+if ( ! function_exists('array_get'))
+{
+    /**
+     * Get an item from an array using "dot" notation.
+     *
+     * @param  array   $array
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    function array_get($array, $key, $default = null)
+    {
+        if (is_null($key)) {
+            return $array;
+        }
+
+        if (isset($array[$key])) {
+            return $array[$key];
+        }
+
+        foreach (explode('.', $key) as $segment) {
+            if ( ! is_array($array) OR ! array_key_exists($segment, $array)) {
+                return $default;
+            }
+
+            $array = $array[$segment];
+        }
+
+        return $array;
+    }
+}
