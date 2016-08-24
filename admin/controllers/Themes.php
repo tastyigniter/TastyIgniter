2d62facf5b749bed851931d0473083a576bada9d
sampoyigi <sam@sampoyigi.com>
2016-08-24 19:19:14 +0200

    Fixed themes add redirect loop
    (cherry picked from commit dd63589)

--- admin/controllers/Themes.php
+++ admin/controllers/Themes.php
@@ -311,10 +311,12 @@ class Themes extends Admin_Controller {
 		return $theme_tree;
 	}
 
-	private function _addTheme() {
+	protected function _addTheme() {
 		$this->user->restrict('Site.Themes.Add', site_url('themes/add'));
 
 		if (isset($_FILES['theme_zip'])) {
+			$this->user->restrict('Site.Themes.Add', site_url('themes/add'));
+
 			if ($this->validateUpload() === TRUE) {
 				$message = $this->Themes_model->extractTheme($_FILES['theme_zip']);
 
