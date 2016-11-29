058f9ad3fadd7c666131ebdaca2b10490bc910f0
sampoyigi <sam@sampoyigi.com>
2016-11-29 19:56:26 +0100

    New location libraries to extend and extract Location library functions

---
+++ system/tastyigniter/libraries/Location_delivery.php
@@ -0,0 +1,551 @@
+<?php
+/**
+ * TastyIgniter
+ *
+ * An open source online ordering, reservation and management system for restaurants.
+ *
+ * @package       TastyIgniter
+ * @author        SamPoyigi
+ * @copyright (c) 2013 - 2016. TastyIgniter
+ * @link          http://tastyigniter.com
+ * @license       http://opensource.org/licenses/GPL-3.0 The GNU GENERAL PUBLIC LICENSE
+ * @since         File available since Release 1.0
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Location Delivery Class
+ *
+ * @category       Libraries
+ * @package        TastyIgniter\Libraries\Location_delivery.php
+ * @link           http://docs.tastyigniter.com
+ */
+class Location_delivery
+{
+	static $areaColors = [];
+	static $chargeSummaryText = [
+		'all'          => '%s on all orders',
+		'above'        => '%s above %s',
+		'below'        => '%s below %s',
+		'free'         => 'Free Delivery',
+		'no_condition' => 'not available %s %s',
+		'no_total'     => 'No Min. Order Amount',
+		'prefix'       => 'Delivery charge: %s',
+	];
+
+	public $allAreas = [];
+	public $nearestArea = null;
+	public $userPosition = null;
+	public $cartTotal = null;
+	public $areaChanged = null;
+
+	public function __construct()
+	{
+		$this->CI =& get_instance();
+
+		self::$areaColors = ['#F16745', '#FFC65D', '#7BC8A4', '#4CC3D9', '#93648D', '#404040', '#F16745', '#FFC65D', '#7BC8A4', '#4CC3D9', '#93648D', '#404040', '#F16745', '#FFC65D', '#7BC8A4', '#4CC3D9', '#93648D', '#404040', '#F16745', '#FFC65D'];
+		$this->userPosition = $this->locationGeocode()->getDefaultPosition();
+		$this->nearestArea = $this->getDefaultAreaBoundary();
+	}
+
+	public static function getChargeSummaryText($item = null)
+	{
+		if (!is_null($item))
+			return isset(self::$chargeSummaryText[$item]) ? self::$chargeSummaryText[$item] : null;
+
+		return self::$chargeSummaryText;
+	}
+
+	public static function setChargeSummaryText(array $chargeSummaryText)
+	{
+		self::$chargeSummaryText = array_merge(self::$chargeSummaryText, $chargeSummaryText);
+	}
+
+	public function getChargeDeliveryAmount()
+	{
+		return $this->checkChargeCondition('delivery');
+	}
+
+	public function getChargeMinOrderTotal()
+	{
+		return $this->checkChargeCondition('min_total');
+	}
+
+	public function findAndSetNearestArea()
+	{
+		if ($area = $this->findNearestArea())
+			$this->setNearestArea($area);
+
+		if ($this->nearestAreaIsEmpty())
+			$this->setDefaultNearestArea();
+
+		return $area;
+	}
+
+	public function findNearestArea()
+	{
+		$nearestArea = null;
+		$nearestAreaBoundary = $this->getDefaultAreaBoundary();
+
+		$userPosition = $this->getUserPosition();
+		if (is_null($userPositionPoint = $this->getUserPositionPoint($userPosition)))
+			return $nearestArea;
+
+		$sessionArea = $this->getSessionNearestArea();
+		if ($this->validateSessionNearestArea($sessionArea, $userPosition))
+			return $sessionArea;
+
+		foreach ($this->getAllBoundaries() as $locationId => $deliveryArea) {
+			foreach ($deliveryArea as $areaId => $boundaries) {
+				$positionBoundary = $this->locationGeocode()->findPositionInBoundaries($userPositionPoint, $boundaries);
+				if ($positionBoundary != 'outside') {
+					$nearestAreaBoundary->location_id = $locationId;
+					$nearestAreaBoundary->area_id = $areaId;
+					$nearestAreaBoundary->position = $positionBoundary;
+					break 2;
+				}
+			}
+		}
+
+		return $nearestAreaBoundary;
+	}
+
+	public function checkCoverage()
+	{
+		$coverage = null;
+		$locationId = $this->getLocation()->getId();
+
+		if (is_null($nearestArea = $this->getNearestArea()))
+			$nearestArea = $this->findNearestArea();
+
+		if (empty($nearestArea->location_id) OR empty($nearestArea->position))
+			return $coverage;
+
+		if ($nearestArea->position != 'outside' AND $nearestArea->location_id == $locationId)
+			return $nearestArea;
+
+		return $coverage;
+	}
+
+	public function calculateDistanceToLocation()
+	{
+		$userPosition = $this->getUserPosition();
+		$userPositionPoint = $this->getUserPositionPoint($userPosition);
+
+		$localPositionPoint = $this->getLocalPosition();
+
+		return $this->locationGeocode()->calculateDistance($userPositionPoint, $localPositionPoint);
+	}
+
+	public function checkChargeCondition($by = 'delivery')
+	{
+		$by = $by == 'delivery' ? 'amount' : 'total';
+		$nearestAreaCharge = $this->getNearestAreaCharge();
+
+		$conditions = array_column($nearestAreaCharge, 'condition');
+		$minTotals = array_column($nearestAreaCharge, 'total');
+
+		$charge = null;
+		// use lowest minimum total when ONLY 'above' condition exist
+		if (in_array('above', $conditions) AND !in_array('below', $conditions) AND !in_array('all', $conditions)) {
+			if ($by == 'total') return min($minTotals);
+		}
+
+		// no minimum total when ONLY 'below' condition exist
+		if (empty($charge) AND in_array('below', $conditions) AND !in_array('below', $conditions) AND !in_array('all', $conditions)) {
+			array_multisort($minTotals, SORT_DESC, $nearestAreaCharge);
+			if ($by == 'total') return 0;
+		}
+
+		// return delivery/minimum total from all 'below' conditions when charge total is greater than/equals cart total
+		if (empty($charge) AND in_array('below', $conditions)) {
+			array_multisort($minTotals, SORT_ASC, $nearestAreaCharge);
+			$charge = $this->checkCartTotal($nearestAreaCharge, 'below');
+		}
+
+		// return delivery/minimum total from all 'above' conditions when charge total is less than/equals cart total
+		if (empty($charge) AND in_array('above', $conditions)) {
+			array_multisort($minTotals, SORT_DESC, $nearestAreaCharge);
+			$charge = $this->checkCartTotal($nearestAreaCharge, 'above');
+		}
+
+		// when all condition exist use the first matching charge
+		if (empty($charge) AND in_array('all', $conditions)) {
+			array_multisort($minTotals, SORT_DESC, $nearestAreaCharge);
+			$charge = $this->checkCartTotal($nearestAreaCharge, 'all');
+		}
+
+		// no minimum total is the found charge condition is 'below'
+		if ($by == 'total' AND $charge['condition'] == 'below') return 0;
+
+		return isset($charge[$by]) ? $charge[$by] : $charge;
+	}
+
+	public function checkCartTotal($nearestAreaCharge, $condition)
+	{
+		$returnCharge = null;
+		$cartTotal = $this->getCartTotal();
+		foreach ($nearestAreaCharge as $charge) {
+			if ($charge['condition'] == $condition) {
+				if ($condition == 'all')
+					return $charge;
+
+				if ($condition == 'below') {
+					if ((float)$charge['total'] >= $cartTotal)
+						return $charge;
+				} else if ($condition == 'above') {
+					if ((float)$charge['total'] <= $cartTotal)
+						return $charge;
+				}
+			}
+		}
+
+		return $returnCharge;
+	}
+
+	public function getNearestAreaCharge()
+	{
+		$nearestArea = $this->getNearestArea(TRUE);
+		if (!isset($nearestArea['charge']))
+			return null;
+
+		return $nearestArea['charge'];
+	}
+
+	public function getNearestAreaChargeSummary()
+	{
+		$nearestArea = $this->getNearestArea(TRUE);
+		if (!isset($nearestArea['full_summary']))
+			return null;
+
+		return $nearestArea['full_summary'];
+	}
+
+	public function getAreas()
+	{
+		$locationId = $this->getLocation()->getId();
+
+		return (isset($this->getAllAreas()[$locationId]) AND is_array($this->getAllAreas()[$locationId])) ?
+			$this->getAllAreas()[$locationId] : [];
+	}
+
+	public function getAllAreas()
+	{
+		$this->setOrCreateAllAreas();
+
+		return $this->allAreas;
+	}
+
+	public function getAllBoundaries()
+	{
+		$boundaries = [];
+
+		foreach ($this->getAllAreas() as $locationId => $deliveryAreas) {
+			foreach ($deliveryAreas as $areaId => $deliveryArea) {
+				$boundaries[$locationId][$areaId] = isset($deliveryArea['polygon']) ? $deliveryArea['polygon'] : [];
+			}
+		}
+
+		return $boundaries;
+	}
+
+	public function setOrCreateAllAreas($allAreas = null)
+	{
+		$allAreas = empty($allAreas) ? $this->allAreas : $allAreas;
+
+		if (empty($allAreas))
+			$allAreas = $this->createAllAreas();
+
+		$this->allAreas = $allAreas;
+
+		return $this;
+	}
+
+	public function createAllAreas()
+	{
+		$areas = [];
+
+		foreach ($this->getByLocationAreas() as $locationId => $deliveryAreas) {
+			foreach ($deliveryAreas as $areaId => $deliveryArea) {
+				$deliveryArea['color'] = $this->getAreaColor($areaId);
+				$deliveryArea['charge'] = $this->createCharges($deliveryArea);
+				$deliveryArea['polygon'] = $this->createPolygons($deliveryArea);
+				$deliveryArea['full_summary'] = $this->createChargeFullSummary($deliveryArea);
+
+				$areas[$locationId][$areaId] = array_merge(['area_id' => $areaId], $deliveryArea);
+			}
+		}
+
+		return $areas;
+	}
+
+	public function createCharges($deliveryArea)
+	{
+		// backward compatibility, remove later
+		if (isset($deliveryArea['charge']) AND is_string($deliveryArea['charge'])) {
+			$deliveryArea['charge'] = [1 => [
+				'amount'    => $deliveryArea['charge'],
+				'condition' => isset($deliveryArea['condition']) ? $deliveryArea['condition'] : 'above',
+				'total'     => (isset($deliveryArea['min_amount'])) ? $deliveryArea['min_amount'] : '0',
+			]];
+		}
+
+		$deliveryCharges = [];
+		foreach ($deliveryArea['charge'] as $key => $charge) {
+			$charge['amount'] = isset($charge['amount']) ? $charge['amount'] : '0';
+			$charge['total'] = isset($charge['total']) ? $charge['total'] : '0';
+			$charge['condition'] = ($charge['total'] <= 0) ? 'all' : $charge['condition'];
+			$charge['rule'] = "{$charge['amount']}|{$charge['condition']}|{$charge['total']}";
+			$charge['summary'] = $this->createChargeSummary($charge);
+
+			$deliveryCharges[$key] = $charge;
+		}
+
+		return $deliveryCharges;
+	}
+
+	public function createChargeFullSummary($deliveryArea)
+	{
+		$nearestAreaCharge = $deliveryArea['charge'];
+		$minTotals = array_column($nearestAreaCharge, 'total');
+		$deliveryAmounts = array_column($nearestAreaCharge, 'amount');
+		$conditions = array_column($nearestAreaCharge, 'condition');
+
+		if (in_array('0', $deliveryAmounts)) array_multisort($deliveryAmounts, SORT_ASC, $nearestAreaCharge);
+
+		$summaries = array_column($nearestAreaCharge, 'summary');
+
+		if (in_array('above', $conditions) AND !in_array('below', $conditions) AND !in_array('all', $conditions)) {
+			array_push($summaries, sprintf($this->getChargeSummaryText('no_condition'), 'below',
+				$this->CI->currency->format(min($minTotals))
+			));
+		}
+
+		$prefix = FALSE;
+		if (in_array('below', $conditions) AND !in_array('above', $conditions) AND !in_array('all', $conditions)) {
+			array_unshift($summaries, sprintf($this->getChargeSummaryText('above'), $this->getChargeSummaryText('free'),
+				$this->CI->currency->format(max($minTotals))
+			));
+		} else {
+			if (!in_array('0', $deliveryAmounts)) $prefix = TRUE;
+		}
+
+		$fullSummary = implode(', ', $summaries);
+
+		if ($prefix)
+			$fullSummary = sprintf($this->getChargeSummaryText('prefix'), $fullSummary);
+
+		return ucfirst(strtolower($fullSummary));
+	}
+
+	public function createChargeSummary($charge)
+	{
+		if (!isset($this->CI->currency))
+			$this->CI->load->library('currency');
+
+		$summary = null;
+
+		$delivery = ($charge['amount'] > 0) ? $this->CI->currency->format($charge['amount']) : $this->getChargeSummaryText('free');
+		$total = ($charge['total'] > 0) ? $this->CI->currency->format($charge['total']) : $this->getChargeSummaryText('no_total');
+
+		if ($charge['condition'] == 'all') {
+			$summary = sprintf($this->getChargeSummaryText('all'), $delivery);
+		} else if ($charge['condition'] == 'above') {
+			$summary = sprintf($this->getChargeSummaryText('above'), $delivery, $total);
+		} else if ($charge['condition'] == 'below') {
+			$summary = sprintf($this->getChargeSummaryText('below'), $delivery, $total);
+		}
+
+		return strtolower(trim(trim($summary, ',')));
+	}
+
+	public function createPolygons($deliveryArea)
+	{
+		$polygons = [];
+
+		$vertices = (!empty($deliveryArea['vertices'])) ? json_decode($deliveryArea['vertices']) : [];
+		$circle = (!empty($deliveryArea['circle'])) ? json_decode($deliveryArea['circle']) : [];
+
+		foreach ($vertices as $vertex) {
+			$polygons['vertices'][] = ['x' => $vertex->lng, 'y' => $vertex->lat];
+		}
+
+		if (isset($circle[0]->center) AND isset($circle[1]->radius)) {
+			$center = $circle[0]->center;
+			$polygons['circle'] = ['center' => $center->lat . '|' . $center->lng, 'radius' => $circle[1]->radius];
+		}
+
+		$polygons['type'] = $deliveryArea['type'];
+
+		return $polygons;
+	}
+
+	/**
+	 * @return Location null
+	 */
+	protected function getLocation()
+	{
+		return $this->CI->location;
+	}
+
+	/**
+	 * @return \Location_geocode
+	 */
+	protected function locationGeocode()
+	{
+		if (isset($this->CI->location_geocode))
+			return $this->CI->location_geocode;
+
+		$this->CI->load->library('location_geocode');
+
+		return $this->CI->location_geocode;
+	}
+
+	public function getAreaColor($areaId)
+	{
+		return isset(self::$areaColors[$areaId - 1]) ? self::$areaColors[$areaId - 1] : '#F16745';
+	}
+
+	public function getCartTotal()
+	{
+		return $this->cartTotal;
+	}
+
+	public function setCartTotal($cartTotal)
+	{
+		$this->cartTotal = $cartTotal;
+
+		return $this;
+	}
+
+	// Transform string coordinates into arrays with x and y values
+	public function getUserPositionPoint($userPosition)
+	{
+		if (!is_string($userPosition) AND $userPosition->status == 'OK')
+			return ['y' => $userPosition->lat, 'x' => $userPosition->lng];
+
+		return null;
+	}
+
+	public function getDefaultAreaBoundary()
+	{
+		$boundary = new \stdClass;
+		$boundary->location_id = $boundary->area_id = $boundary->position = null;
+
+		return $boundary;
+	}
+
+	public function getLocalPosition()
+	{
+		$position = [];
+
+		$localInfo = $this->getLocation()->local();
+
+		if (isset($localInfo['location_lat']))
+			$position['y'] = $localInfo['location_lat'];
+
+		if (isset($localInfo['location_lng']))
+			$position['x'] = $localInfo['location_lng'];
+
+		return $position;
+	}
+
+	public function getUserPosition()
+	{
+		return $this->userPosition;
+	}
+
+	public function setUserPosition($userPosition)
+	{
+		$this->userPosition = $userPosition;
+
+		return $this;
+	}
+
+	public function getNearestArea($fullArray = FALSE)
+	{
+		if ($fullArray) {
+			$locationId = $this->getLocation()->getId();
+			$areaId = $this->getNearestAreaId();
+			$allAreas = $this->getAllAreas();
+
+			return (isset($allAreas[$locationId][$areaId])) ? $allAreas[$locationId][$areaId] : null;
+		}
+
+		return $this->nearestArea;
+	}
+
+	public function getNearestAreaId()
+	{
+		return $this->nearestArea->area_id;
+	}
+
+	public function getSessionNearestArea()
+	{
+		$localInfo = empty($localInfo) ? $this->getLocation()->getSessionData() : $localInfo;
+
+		return isset($localInfo['area']) ? $localInfo['area'] : $this->getDefaultAreaBoundary();
+	}
+
+	public function setNearestArea($nearestArea)
+	{
+		$this->nearestArea = $nearestArea;
+	}
+
+	public function setDefaultNearestArea()
+	{
+		$this->nearestArea->location_id = $this->getLocation()->getId();
+		$this->nearestArea->area_id = ($deliveryAreas = $this->getAreas()) ? key($deliveryAreas) : 1;
+		$this->nearestArea->position = 'inside';
+	}
+
+	public function nearestAreaIsEmpty()
+	{
+		foreach ((array)$this->getNearestArea() as $item) {
+			if (!is_null($item))
+				return FALSE;
+		}
+
+		return TRUE;
+	}
+
+	public function validateSessionNearestArea($sessionArea, $userPosition)
+	{
+		if (empty($sessionArea->location_id) OR empty($userPosition->search_query))
+			return FALSE;
+
+		$locationId = $this->getLocation()->getId();
+		$sessionPosition = $this->locationGeocode()->getSessionPosition();
+		if (!empty($sessionPosition->search_query) AND $sessionPosition->search_query == $userPosition->search_query)
+			return $sessionArea;
+
+		$nearestArea = $this->getNearestArea();
+
+		if (!is_null($nearestArea->area_id) AND $sessionArea->location_id == $locationId AND $sessionArea->area_id == $nearestArea->area_id)
+			return $sessionArea;
+
+		if (!is_null($sessionArea->location_id) AND $sessionArea == $userPosition)
+			return TRUE;
+
+		return FALSE;
+	}
+
+	protected function getByLocationAreas()
+	{
+		$allAreas = [];
+
+		foreach ($this->getByLocationsId() as $locationId => $location) {
+			if (isset($location['options']['delivery_areas']))
+				$allAreas[$locationId] = $location['options']['delivery_areas'];
+		}
+
+		return $allAreas;
+	}
+
+	protected function getByLocationsId()
+	{
+		$locationModels = $this->getLocation()->getLocationModels();
+
+		return !is_null($locationModels) ? $locationModels->keyBy('location_id')->all() : [];
+	}
+}
---
+++ system/tastyigniter/libraries/Location_hours.php
@@ -0,0 +1,336 @@
+<?php
+/**
+ * TastyIgniter
+ *
+ * An open source online ordering, reservation and management system for restaurants.
+ *
+ * @package       TastyIgniter
+ * @author        SamPoyigi
+ * @copyright (c) 2013 - 2016. TastyIgniter
+ * @link          http://tastyigniter.com
+ * @license       http://opensource.org/licenses/GPL-3.0 The GNU GENERAL PUBLIC LICENSE
+ * @since         File available since Release 1.0
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Location Working Hours Class
+ *
+ * @category       Libraries
+ * @package        TastyIgniter\Libraries\Location_hours.php
+ * @link           http://docs.tastyigniter.com
+ */
+class Location_hours
+{
+	const CLOSED = 'closed';
+	const OPEN = 'open';
+	const OPENING = 'opening';
+
+	public $workingType = null;
+	public $weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+	public $currentDay = null;
+	public $currentTime = null;
+	public $dateFormat = "%d/%m/%Y";
+	public $timeFormat = "%H:%i";
+	public $weekDay = null;
+	protected $allWorkingHours = [];
+	public $workingSchedule = [];
+
+	public function __construct()
+	{
+		$this->CI =& get_instance();
+		$this->CI->load->database();
+		$this->CI->load->model('Working_hours_model');
+
+		$this->currentDay = date('l');
+		$this->currentTime = time();
+
+		$this->setDateTimeFormat();
+		$this->setWeekDay(mdate('%N', time()));
+	}
+
+	public function getHours()
+	{
+		$workingHours = $this->getByLocation();
+
+		foreach ($workingHours as $type => &$workingHour) {
+			foreach ($workingHour as $day => &$hour) {
+				$hour['open'] = mdate($this->getTimeFormat(), $hour['open']);
+				$hour['close'] = mdate($this->getTimeFormat(), $hour['close']);
+			}
+		}
+
+		return $workingHours;
+	}
+
+	public function getWorkingSchedule()
+	{
+		if (empty($this->workingSchedule))
+			$this->setWorkingSchedule();
+
+		return $this->workingSchedule;
+	}
+
+	public function setWorkingSchedule()
+	{
+		$currentTime = $this->getLocation()->currentTime();
+		$startDate = mdate("%d-%m-%Y", strtotime("-1 day", $currentTime));
+		$endDate = mdate("%d-%m-%Y", strtotime("+7 day", $currentTime));
+
+		$this->workingSchedule = $this->createSchedule($startDate, $endDate, TRUE);
+	}
+
+	public function getOrderSchedule($startDate, $endDate)
+	{
+		$workingSchedule = $this->createSchedule($startDate, $endDate);
+
+		$timeInterval = $this->orderTimeInterval();
+		$lastOrderTime = $this->lastOrderTime();
+		$currentTime = $this->getLocation()->currentTime();
+
+		$count = 1;
+		$scheduleRange = [];
+		foreach ($workingSchedule as $date => $workingHour) {
+
+			$startTime = mdate("%d-%m-%Y %H:%i", $workingHour['open'] + ($timeInterval * 60));
+			$endTime = mdate("%d-%m-%Y %H:%i", $workingHour['close'] - $lastOrderTime);
+			$timeRange = $this->getTimeRange($startTime, $endTime);
+
+			foreach ($timeRange as $time) {
+				if (strtotime($time) >= $currentTime + ($timeInterval * 60)) {
+					if ($workingHour['working_status'] === 'open' AND $count === 1) {
+						$scheduleRange['asap'] = mdate('%d-%m-%Y %H:%i', $currentTime + ($timeInterval * 60));
+					} else {
+						$date = mdate('%d-%m-%Y', strtotime($time));
+						$hour = mdate('%H', strtotime($time));
+						$scheduleRange[$date][$hour][] = mdate('%i', strtotime($time));
+					}
+
+					$count++;
+				}
+			}
+		}
+
+		return $scheduleRange;
+	}
+
+	public function getTimeRange($startTime, $endTime)
+	{
+		return time_range($startTime, $endTime, $this->orderTimeInterval(), "%d-%m-%Y %H:%i");
+	}
+
+	public function createSchedule($startDate, $endDate, $returnFirstOpen = FALSE)
+	{
+		$result = [];
+
+		while (strtotime($startDate) <= strtotime($endDate)) {
+			$day = mdate('%N', strtotime($startDate)) - 1;
+
+			if ($workingHour = $this->setWeekDay($day)->getByLocationWorkingTypeDay()) {
+				$close = strtotime("{$startDate} {$workingHour['closing_time']}");
+				$workingHour['open'] = strtotime("{$startDate} {$workingHour['opening_time']}");
+				$workingHour['till_next_day'] = ($workingHour['open'] > $close) ? TRUE : FALSE;
+				$workingHour['close'] = ($workingHour['till_next_day']) ? $close + 86400 : $close;
+
+				$workingHour['working_status'] = $this->checkStatus(null, $workingHour);
+
+				if ($returnFirstOpen AND $workingHour['working_status'] != 'closed') return $workingHour;
+
+				$result[$startDate] = $workingHour;
+			}
+
+			$startDate = mdate("%d-%m-%Y", strtotime("+1 day", strtotime($startDate)));
+		}
+
+		return $result;
+	}
+
+	public function getWorkingTime($hourType, $formatTime = TRUE)
+	{
+		$workingSchedule = $this->getWorkingSchedule();
+
+		$workingTime = null;
+
+		if (empty($workingSchedule) OR empty($workingSchedule[$hourType]))
+			return $workingTime;
+
+		$timeFormat = $this->getTimeFormat();
+		if ($workingSchedule['till_next_day'] === TRUE) {
+			$timeFormat = '%D ' . $this->getTimeFormat();
+		}
+
+		return $formatTime ? mdate($timeFormat, $workingSchedule[$hourType]) : $workingSchedule[$hourType];
+	}
+
+	public function checkStatus($currentTime = null, $workingSchedule = [])
+	{
+		$workingStatus = self::CLOSED;
+
+		$workingSchedule = (array)$workingSchedule ? $workingSchedule : $this->getWorkingSchedule();
+		$currentTime = !empty($currentTime) ? strtotime($currentTime) : $this->getLocation()->currentTime();
+
+		if (!isset($workingSchedule['open'], $workingSchedule['close']))
+			return $workingStatus;
+
+		$openTime = $workingSchedule['open'];
+		$closeTime = $workingSchedule['close'];
+
+		if (in_array($workingSchedule['type'], ['delivery', 'collection']))
+			$closeTime = $closeTime - $this->lastOrderTime();
+
+		if ($workingSchedule['status'] == '1' AND $currentTime <= $closeTime)
+			$workingStatus = ($currentTime >= $openTime) ? self::OPEN : self::OPENING;
+
+		return $workingStatus;
+	}
+
+	/**
+	 * @return array|mixed
+	 */
+	protected function getByLocation()
+	{
+		if (empty($this->allWorkingHours))
+			$this->allWorkingHours = $this->getModel()->getHoursByLocation();
+
+		return isset($this->allWorkingHours[$this->getLocation()->getId()]) ?
+			$this->allWorkingHours[$this->getLocation()->getId()] : [];
+	}
+
+	/**
+	 * @return array|mixed
+	 */
+	protected function getByLocationWorkingType()
+	{
+		$workingHours = $this->getByLocation();
+
+		return isset($workingHours[$this->getWorkingType()]) ? $workingHours[$this->getWorkingType()] : [];
+	}
+
+	/**
+	 * @return array|mixed
+	 */
+	protected function getByLocationWorkingTypeDay()
+	{
+		$workingHours = $this->getByLocationWorkingType();
+
+		return isset($workingHours[$this->getWeekDay()]) ? $workingHours[$this->getWeekDay()] : [];
+	}
+
+	/**
+	 * @return Working_hours_model
+	 */
+	public function getModel()
+	{
+		return $this->CI->Working_hours_model;
+	}
+
+	public function getWorkingType()
+	{
+		return is_null($this->workingType) ? 'delivery' : $this->workingType;
+	}
+
+	public function setWorkingType($workingType)
+	{
+		$this->workingType = $workingType;
+
+		return $this;
+	}
+
+	/**
+	 * @return Location null
+	 */
+	public function getLocation()
+	{
+		return $this->CI->location;
+	}
+
+	public function getCurrentTime()
+	{
+		return $this->currentTime;
+	}
+
+	public function setCurrentTime($currentTime)
+	{
+		$this->currentTime = $currentTime;
+
+		return $this;
+	}
+
+	public function getCurrentDay()
+	{
+		return $this->currentDay;
+	}
+
+	public function setCurrentDay($currentDay)
+	{
+		$this->currentDay = $currentDay;
+
+		return $this;
+	}
+
+	public function getWeekDay()
+	{
+		return $this->weekDay;
+	}
+
+	public function setWeekDay($weekDay)
+	{
+		$this->weekDay = $weekDay;
+
+		return $this;
+	}
+
+	public function setDateTimeFormat($dateFormat = null, $timeFormat = null)
+	{
+		$this->dateFormat = (is_null($dateFormat)) ? $this->CI->config->item('date_format') : $dateFormat;
+		$this->timeFormat = (is_null($timeFormat)) ? $this->CI->config->item('time_format') : $timeFormat;
+
+		return $this;
+	}
+
+	public function setDateFormat($dateFormat)
+	{
+		$this->dateFormat = $dateFormat;
+
+		return $this;
+	}
+
+	public function getDateFormat()
+	{
+		return $this->dateFormat;
+	}
+
+	public function getTimeFormat()
+	{
+		return $this->timeFormat;
+	}
+
+	public function setTimeFormat($timeFormat)
+	{
+		$this->timeFormat = $timeFormat;
+
+		return $this;
+	}
+
+	public function getWeekDayText()
+	{
+		return $this->getModel()->getWeekDays()[$this->getWeekDay()];
+	}
+
+	public function lastOrderTime()
+	{
+		$lastOrderTime = !is_null($this->getLocation()->local()['last_order_time']) ?
+			$this->getLocation()->local()['last_order_time'] : 0;
+
+		return (is_numeric($lastOrderTime) AND $lastOrderTime > 0) ? $lastOrderTime * 60 : $lastOrderTime;
+	}
+
+	public function orderTimeInterval()
+	{
+		if ($this->getLocation()->orderType() == '1') {
+			return $this->getLocation()->deliveryTime();
+		} else {
+			return $this->getLocation()->collectionTime();
+		}
+	}
+}
