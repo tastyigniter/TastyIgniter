04231def188a05bb950e7f54c7e10bc6da9fd113
SamPoyigi <sam@sampoyigi.com>
2018-07-07 20:16:30 +0100

    Minor ui improvements

--- themes/demo/_partials/head.php
+++ themes/demo/_partials/head.php
@@ -1,6 +1,6 @@
 <?= get_metas(); ?>
 <link href="<?= image_url('favicon.ico'); ?>" rel="shortcut icon" type="image/ico">
 <title><?= sprintf(lang('main::lang.site_title'), lang(get_title()), setting('site_name')); ?></title>
-<link href="/app/system/assets/ui/flame.css" rel="stylesheet" type="text/css" id="flame-css">
+<link href="<?= asset('app/system/assets/ui/flame.css') ?>" rel="stylesheet" type="text/css" id="flame-css">
 <?= get_style_tags(['widget', 'component', 'theme']); ?>
-<link href="<?= theme_url('demo/assets/css/demo.css') ?>" rel="stylesheet" type="text/css" id="stylesheet-css">
+<link href="<?= theme_url('demo/assets/css/demo.css') ?>" rel="stylesheet" type="text/css" id="demo-css">
