4971fed01af01e6559313e47d64663c9aee95c5b
SamPoyigi <sam@sampoyigi.com>
2018-07-07 22:46:40 +0100

    Fix scss url rewrite issue

--- app/system/views/extensions/lists/extension_card.php
+++ app/system/views/extensions/lists/extension_card.php
@@ -1,10 +1,3 @@
-<?php
-$componentNames = [];
-if ($record->class) foreach ($record->class->registerComponents() as $component) {
-    if (!isset($component['name'])) continue;
-    $componentNames[] = lang($component['name']);
-}
-?>
 <h5 class="extension-name<?= (!$record->class) ? ' text-muted' : ''; ?>">
     <?php if ($record->class) { ?>
         <?= $record->title; ?>
@@ -12,13 +5,5 @@ if ($record->class) foreach ($record->class->registerComponents() as $component)
     else { ?>
         <s><?= $record->title; ?></s>&nbsp;&nbsp;
     <?php } ?>
-    <?php if (count($componentNames)) { ?>
-        <span class="">
-            <i
-                class="extension-label fa fa-cubes"
-                title="<?= implode(PHP_EOL, $componentNames); ?>"
-            ></i>
-        </span>
-    <?php } ?>
 </h5>
-<p class="extension-desc text-muted"><?= $record->meta['description']; ?></p>
+<span class="extension-desc text-muted"><?= $record->meta['description']; ?></span>
