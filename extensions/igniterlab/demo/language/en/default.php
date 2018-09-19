d5ce892d87fa2419d23a1693ee47ee7d9cd5cc61
SamPoyigi <sam@sampoyigi.com>
2018-09-19 16:59:15 +0100

    Rename demo extension namespace

--- extensions/igniterlab/demo/language/en/default.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    '_text_title'          => 'Demo Component',
-    'text_tab_general'     => 'General',
-    'text_component_title' => 'Demo Component',
-    'text_component_desc'  => 'Component provided for demonstration',
-];
