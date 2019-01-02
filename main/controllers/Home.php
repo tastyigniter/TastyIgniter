0eebb279837afcb0c0535bd64718d2f5b43ec1b9
Saravanan Shivaji <11622323+saravananshivaji@users.noreply.github.com>
2019-01-02 18:52:31 +0530

    Google places auto complete

--- main/controllers/Home.php
+++ main/controllers/Home.php
@@ -4,7 +4,14 @@ class Home extends Main_Controller {
 
 	public function index() {
         $this->lang->load('home');
-
+        
+        if ($this->config->item('maps_api_key')) {
+            $map_key = '&key=' . $this->config->item('maps_api_key');
+        } else {
+            $map_key = '';
+        }
+        
+        $this->template->setScriptTag('https://maps.googleapis.com/maps/api/js?v=3' .$map_key.'&libraries=geometry,places', 'google-maps-js', '104330');
         $this->template->setTitle($this->lang->line('text_heading'));
 
 		$this->template->render('home');
