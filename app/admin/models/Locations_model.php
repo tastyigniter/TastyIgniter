dcada3f0389075a20019adf1c393914e504ca111
SamPoyigi <sam@sampoyigi.com>
2019-01-27 14:13:55 +0000

    Move location hours & areas related logic from model to trait

--- app/admin/models/Locations_model.php
+++ app/admin/models/Locations_model.php
@@ -224,58 +224,9 @@ class Locations_model extends AbstractLocation
     {
         $value = @unserialize($this->attributes['options']) ?: [];
 
-        // Rename options array index 'opening_hours' to 'hours'
-        if (isset($value['opening_hours'])) {
-            $hours = $value['opening_hours'];
-            foreach (['opening', 'daily', 'delivery', 'collection'] as $type) {
-                foreach (['type', 'days', 'hours'] as $suffix) {
-                    if (isset($hours["{$type}_{$suffix}"])) {
-                        $valueItem = $hours["{$type}_{$suffix}"];
-                        if ($suffix == 'type')
-                            $valueItem = $valueItem != '24_7' ? $valueItem : '24_7';
+        $this->parseHoursFromOptions($value);
 
-                        $typeIndex = $type == 'daily' ? 'opening' : $type;
-
-                        if ($suffix == 'hours') {
-                            $value['hours'][$typeIndex]['open'] = $valueItem['open'] ?? '00:00';
-                            $value['hours'][$typeIndex]['close'] = $valueItem['close'] ?? '23:59';
-                        }
-                        else {
-                            $value['hours'][$typeIndex][$suffix] = $valueItem;
-                        }
-                    }
-                }
-            }
-
-            if (isset($hours['flexible_hours']) AND is_array($hours['flexible_hours'])) {
-                foreach (['opening', 'delivery', 'collection'] as $type) {
-                    $value['hours'][$type]['flexible'] = $hours['flexible_hours'];
-                }
-            }
-
-            unset($value['opening_hours']);
-        }
-
-        // Ensures form checkbox is unchecked when value is empty
-        foreach (['opening', 'delivery', 'collection'] as $type) {
-            if (!isset($value['hours'][$type]['days']))
-                $value['hours'][$type]['days'] = [];
-        }
-
-        // Rename options array index ['delivery_areas']['charge']
-        // to ['delivery_areas']['conditions']
-        if (isset($value['delivery_areas'])) {
-            foreach ($value['delivery_areas'] as &$area) {
-                if (!isset($charge['charge'])) continue;
-                $area['conditions'] = is_array($area['charge']) ? $area['charge'] : [];
-                foreach ($area['conditions'] as $id => &$charge) {
-                    if (!isset($charge['condition'])) continue;
-                    $charge['type'] = $charge['condition'];
-                    unset($charge['condition']);
-                }
-                unset($area['charge']);
-            }
-        }
+        $this->parseAreasFromOptions($value);
 
         $this->attributes['options'] = @serialize($value);
 
@@ -352,84 +303,6 @@ class Locations_model extends AbstractLocation
     }
 
     /**
-     * Create a new or update existing location working hours
-     *
-     * @param array $data
-     *
-     * @return bool
-     */
-    public function addOpeningHours($data = [])
-    {
-        $created = FALSE;
-
-        $this->working_hours()->delete();
-
-        if (!$data)
-            return FALSE;
-
-        foreach ($data as $type => $hours) {
-            $hoursArray = [];
-
-            if (!isset($hours['type'])) continue;
-
-            switch ($hourType = $hours['type']) {
-                case '24_7':
-                    $hoursArray = $this->createWorkingHoursArray($hourType, $hours);
-                    break;
-                case 'daily':
-                    $hoursArray = $this->createWorkingHoursArray($hourType, $hours);
-                    break;
-                case 'flexible':
-                    $hoursArray = $this->createWorkingHoursArray($hourType, $hours);
-                    break;
-            }
-
-            foreach ($hoursArray as $hourValue) {
-                $created = $this->working_hours()->create([
-                    'location_id' => $this->getKey(),
-                    'weekday' => $hourValue['day'],
-                    'type' => $type,
-                    'opening_time' => mdate('%H:%i', strtotime($hourValue['open'])),
-                    'closing_time' => mdate('%H:%i', strtotime($hourValue['close'])),
-                    'status' => $hourValue['status'],
-                ]);
-            }
-        }
-
-        return $created;
-    }
-
-    /**
-     * Create a new or update existing location areas
-     *
-     * @param array $deliveryAreas
-     *
-     * @return bool
-     */
-    public function addLocationAreas($deliveryAreas)
-    {
-        $locationId = $this->getKey();
-        if (!is_numeric($locationId))
-            return FALSE;
-
-        if (!is_array($deliveryAreas))
-            return FALSE;
-
-        foreach ($deliveryAreas as $area) {
-            $locationArea = $this->delivery_areas()->firstOrNew([
-                'area_id' => $area['area_id'] ?? null,
-            ])->fill(array_except($area, ['area_id']));
-
-            $locationArea->save();
-            $idsToKeep[] = $locationArea->getKey();
-        }
-
-        $this->delivery_areas()->whereNotIn('area_id', $idsToKeep)->delete();
-
-        return count($idsToKeep);
-    }
-
-    /**
      * Create a new or update existing location tables
      *
      * @param array $tables
@@ -439,37 +312,5 @@ class Locations_model extends AbstractLocation
     public function addLocationTables($tables = [])
     {
         return $this->tables()->sync($tables);
-    }
-
-    /**
-     * Build working hours array
-     *
-     * @param $type
-     * @param $data
-     *
-     * @return array
-     */
-    public function createWorkingHoursArray($type, $data)
-    {
-        $hours = ['open' => '00:00', 'close' => '23:59', 'status' => 1];
-        if ($type != '24_7')
-            $hours = ['open' => $data['open'], 'close' => $data['close']];
-
-        $days = isset($data['days']) ? $data['days'] : [];
-
-        $workingHours = [];
-
-        for ($day = 0; $day <= 6; $day++) {
-            $_hours = ($type == 'flexible' AND isset($data['flexible'][$day])) ? $data['flexible'][$day] : $hours;
-            $workingHours[] = [
-                'day' => $day,
-                'type' => $type,
-                'open' => $_hours['open'],
-                'close' => $_hours['close'],
-                'status' => isset($_hours['status']) ? $_hours['status'] : (int)in_array($day, $days),
-            ];
-        }
-
-        return $workingHours;
     }
 }
