e7f51a64e8e36802f32a0a10c74d0f93fa200d05
Sam Poyigi <sam@sampoyigi.com>
2020-04-20 00:36:02 +0100

    Make installing demo data optional

--- app/system/database/seeds/DemoSchemaSeeder.php
+++ app/system/database/seeds/DemoSchemaSeeder.php
@@ -13,7 +13,7 @@ class DemoSchemaSeeder extends Seeder
      */
     public function run()
     {
-        $this->seedDefaultLocation();
+        if (!DatabaseSeeder::$seedDemo) return;
 
         $this->seedCategories();
 
@@ -22,44 +22,6 @@ class DemoSchemaSeeder extends Seeder
         $this->seedMenuItems();
 
         $this->seedCoupons();
-    }
-
-    protected function seedDefaultLocation()
-    {
-        // Abort: a location already exists
-        if (DB::table('locations')->count())
-            return TRUE;
-
-        $location = $this->getSeedRecords('location');
-        $location['location_email'] = DatabaseSeeder::$siteEmail;
-        $location['options'] = serialize($location['options']);
-        $location['delivery_areas'][0]['boundaries']['circle'] = json_encode(
-            $location['delivery_areas'][0]['boundaries']['circle']
-        );
-
-        $locationId = DB::table('locations')->insertGetId(array_except($location, ['delivery_areas']));
-
-        $this->seedLocationTables($locationId);
-    }
-
-    protected function seedLocationTables($locationId)
-    {
-        if (DB::table('tables')->count())
-            return;
-
-        for ($i = 1; $i < 15; $i++) {
-            $tableId = DB::table('tables')->insertGetId([
-                'table_name' => 'Table '.$i,
-                'min_capacity' => random_int(2, 5),
-                'max_capacity' => random_int(6, 12),
-                'table_status' => 1,
-            ]);
-
-            DB::table('location_tables')->insert([
-                'location_id' => $locationId,
-                'table_id' => $tableId,
-            ]);
-        }
     }
 
     protected function seedWorkingHours($locationId)
