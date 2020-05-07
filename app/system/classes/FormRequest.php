d40616243c9fec51e5cdf54ef31af88e259f36f9
Sam Poyigi <sam@sampoyigi.com>
2020-05-07 16:56:38 +0100

    New event to extend validator parameters

--- app/system/classes/FormRequest.php
+++ app/system/classes/FormRequest.php
@@ -6,7 +6,7 @@ use Admin\Actions\FormController;
 use Admin\Classes\AdminController;
 use Igniter\Flame\Exception\SystemException;
 use Igniter\Flame\Exception\ValidationException;
-use Igniter\Flame\Traits\ExtendableTrait;
+use Igniter\Flame\Traits\EventEmitter;
 use Illuminate\Contracts\Validation\Factory;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest as BaseFormRequest;
@@ -16,8 +16,8 @@ use System\Traits\RuleInjector;
 
 class FormRequest extends BaseFormRequest
 {
-    use ExtendableTrait;
     use RuleInjector;
+    use EventEmitter;
 
     protected const DATA_TYPE_FORM = 'form';
     protected const DATA_TYPE_POST = 'post';
@@ -119,23 +119,27 @@ class FormRequest extends BaseFormRequest
      */
     protected function createDefaultValidator(Factory $factory)
     {
-        $rules = $this->container->call([$this, 'rules']);
+        $registeredRules = $this->container->call([$this, 'rules']);
+        $parsedRules = ValidationHelper::prepareRules($registeredRules);
 
-        $parsed = ValidationHelper::prepareRules($rules);
-        $rules = Arr::get($parsed, 'rules', $rules);
-        $messages = Arr::get($parsed, 'messages', $this->messages());
-        $attributes = Arr::get($parsed, 'attributes', $this->attributes());
+        $dataHolder = new \stdClass();
+        $dataHolder->data = $this->validationData();
+        $dataHolder->rules = Arr::get($parsedRules, 'rules', $registeredRules);
+        $dataHolder->messages = Arr::get($parsedRules, 'messages', $this->messages());
+        $dataHolder->attributes = Arr::get($parsedRules, 'attributes', $this->attributes());
 
         if ($this->getInjectRuleParameters()) {
-            $rules = $this->injectParametersToRules($rules);
+            $dataHolder->rules = $this->injectParametersToRules($dataHolder->rules);
         }
 
-        return $factory->make($this->validationData(), $rules, $messages, $attributes);
-    }
+        $this->fireSystemEvent('system.formRequest.extendValidator', [$dataHolder]);
 
-    protected function validationRules()
-    {
-
+        return $factory->make(
+            $dataHolder->data,
+            $dataHolder->rules,
+            $dataHolder->messages,
+            $dataHolder->attributes
+        );
     }
 
     /**
