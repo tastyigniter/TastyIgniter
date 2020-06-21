4b219d713831bb668dbb5d3d796c77242e23bef2
Sam Poyigi <sam@sampoyigi.com>
2020-06-21 13:57:59 +0100

    Use input group for date picker

--- app/admin/widgets/filter/scope_date.php
+++ app/admin/widgets/filter/scope_date.php
@@ -1,12 +1,17 @@
 <div class="filter-scope date form-group">
     <input type="hidden" name="<?= $this->getScopeName($scope) ?>" value="<?= $scope->value ?>" data-datepicker-value="">
-	<input 
-		type="text"
-		id="<?= $this->getScopeName($scope) ?>-datepicker"
-		class="form-control" 
-		autocomplete="off" 
-		value="<?= mdate('%d-%m-%Y', strtotime($scope->value)) ?>" 
-		data-control="datepicker" 
-		data-format="dd-mm-yyyy"
-	>
+    <div class="input-group">
+        <input
+            type="text"
+            id="<?= $this->getScopeName($scope) ?>-datepicker"
+            class="form-control"
+            autocomplete="off"
+            value="<?= mdate('%d-%m-%Y', strtotime($scope->value)) ?>"
+            data-control="datepicker"
+            data-format="dd-mm-yyyy"
+        >
+        <div class="input-group-append">
+            <span class="input-group-icon"><i class="fa fa-calendar"></i></span>
+        </div>
+    </div>
 </div>
