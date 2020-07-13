435fa25e3a62ecb0b957843a4b4598b4fe9bcc6f
Sam Poyigi <sam@sampoyigi.com>
2020-07-13 22:50:42 +0100

    minor ui improvements

--- app/admin/widgets/lists/list_pagination.php
+++ app/admin/widgets/lists/list_pagination.php
@@ -2,7 +2,7 @@
     <nav class="pagination-bar d-flex justify-content-end">
         <?php if ($showPageNumbers) { ?>
             <div class="align-self-center">
-                <?= sprintf(lang('admin::lang.list.text_showing'), $records->firstItem(), $records->lastItem(), $records->total()) ?>
+                <?= sprintf(lang('admin::lang.list.text_showing'), $records->firstItem() ?? 0, $records->lastItem() ?? 0, $records->total()) ?>
             </div>
         <?php } ?>
         <div class="pl-3">
