48a015c3d9e1bcc0afbd4acf77817f24fc3b11c9
Ryan Mitchell <ryan@thoughtcollective.com>
2020-08-20 16:05:47 +0100

    Formatting

--- app/admin/formwidgets/mapview/mapview.php
+++ app/admin/formwidgets/mapview/mapview.php
@@ -5,7 +5,7 @@
     data-map-center="<?= e(json_encode($mapCenter)); ?>"
     data-map-shape-selector="<?= $shapeSelector ?>"
     data-map-editable-shape="<?= !$previewMode ?>"
-	data-map-distance-unit="<?= setting('distance_unit'); ?>"
+    data-map-distance-unit="<?= setting('distance_unit'); ?>"
 >
     <div class="map-view"></div>
 </div>
