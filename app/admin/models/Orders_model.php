ba3c141afa2732f22faea382cfa4282fc7af2273
Ryan Mitchell <ryan@thoughtcollective.com>
2020-08-25 09:44:25 +0100

    Add variables on order model

--- app/admin/models/Orders_model.php
+++ app/admin/models/Orders_model.php
@@ -345,9 +345,11 @@ class Orders_model extends Model
             $optionData = [];
             if ($menuItemOptions = $menuOptions->get($menu->order_menu_id)) {
                 foreach ($menuItemOptions as $menuItemOption) {
-                    $optionData[] = $menuItemOption->order_option_name
-                        .lang('admin::lang.text_equals')
-                        .currency_format($menuItemOption->order_option_price);
+                    $optionData[] = [
+	                    'menu_option_quantity' => $menuItemOption->quantity,
+	                    'menu_option_name' => $menuItemOption->order_option_name,
+	                    'menu_option_price' => $menuItemOption->order_option_price,
+                    ];
                 }
             }
 
