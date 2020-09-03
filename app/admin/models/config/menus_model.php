f02a595530b7d9a5fc736c5237c1fad8898c8971
Sam Poyigi <sam@sampoyigi.com>
2020-09-03 15:43:30 +0100

    Remove placeholder on menus allergens form field

--- app/admin/models/config/menus_model.php
+++ app/admin/models/config/menus_model.php
@@ -165,7 +165,6 @@ $config['form']['tabs'] = [
             'label' => 'lang:admin::lang.menus.label_allergens',
             'type' => 'relation',
             'span' => 'right',
-            'placeholder' => 'lang:admin::lang.text_please_select',
         ],
         'mealtimes' => [
             'label' => 'lang:admin::lang.menus.label_mealtime',
