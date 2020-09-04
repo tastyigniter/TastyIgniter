9e873bf1543684deea02854950da5f3f5d17e427
Ryan Mitchell <ryan@thoughtcollective.com>
2020-09-04 07:55:42 +0100

    Maparea --> Blade

---
+++ app/admin/formwidgets/maparea/maparea.blade.php
@@ -0,0 +1,29 @@
+<div
+    id="{{ $this->getId() }}"
+    data-control="map-area"
+    data-alias="{{ $this->alias }}"
+    data-last-counter="{{ $indexCount }}"
+>
+    <div class="map-area-container my-3">
+        {{ $this->makePartial('maparea/areas') }}
+    </div>
+
+    <div
+        id="{{ $this->getId('toolbar') }}"
+        class="map-area-toolbar"
+    >
+        {{ $this->makePartial('maparea/toolbar') }}
+    </div>
+
+    <div
+        class="modal fade"
+        id="{{ $this->getId('map-modal') }}"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="{{ $this->getId('map-modal-title') }}"
+        aria-hidden="true"
+        data-area-map-modal
+    >
+        {{ $this->makePartial('maparea/map_modal') }}
+    </div>
+</div>
--- app/admin/formwidgets/maparea/toolbar.blade.php
+++ app/admin/formwidgets/maparea/toolbar.blade.php
@@ -9,15 +9,15 @@
                 type="button"
                 class="btn btn-default"
                 data-control="add-area"
-                data-handler="<?= $this->getEventHandler('onAddArea') ?>"
+                data-handler="{{ $this->getEventHandler('onAddArea') }}"
                 data-attach-loading
-            ><i class="fa fa-plus"></i>&nbsp;&nbsp;<?= $prompt ? e(lang($prompt)) : '' ?></button>
+            ><i class="fa fa-plus"></i>&nbsp;&nbsp;{{ $prompt ? lang($prompt) : '' }}</button>
             <button
                 type="button"
                 class="btn btn-outline-default"
                 data-toggle="modal"
-                data-target="#<?= $this->getId('map-modal') ?>"
-            ><i class="fa fa-map"></i>&nbsp;&nbsp;<?= $mapPrompt ? e(lang($mapPrompt)) : '' ?></button>
+                data-target="#{{ $this->getId('map-modal') }}"
+            ><i class="fa fa-map"></i>&nbsp;&nbsp;{{ $mapPrompt ? lang($mapPrompt) : '' }}</button>
         </div>
     </div>
 </div>
