7b1ba0e76009b7562f8c5c1f397eb9df03796bfd
Ryan Mitchell <ryan@thoughtcollective.com>
2020-09-04 07:46:28 +0100

    Datepicker --> Blade

--- app/admin/formwidgets/datepicker/picker_time.blade.php
+++ app/admin/formwidgets/datepicker/picker_time.blade.php
@@ -4,13 +4,13 @@
     data-autoclose="true">
     <input
         type="text"
-        name="<?= $field->getName() ?>"
-        id="<?= $this->getId('time') ?>"
+        name="{{ $field->getName() }}"
+        id="{{ $this->getId('time') }}"
         class="form-control"
         autocomplete="off"
-        value="<?= $value ? $value->format($timeFormat) : null ?>"
-        <?= $field->getAttributes() ?>
-        <?= $this->previewMode ? 'readonly="readonly"' : '' ?>
+        value="{{ $value ? $value->format($timeFormat) : null }}"
+        {{ $field->getAttributes() }}
+        @if($this->previewMode) readonly="readonly" @endif
     />
     <div class="input-group-append">
         <span class="input-group-icon"><i class="fa fa-clock-o"></i></span>
