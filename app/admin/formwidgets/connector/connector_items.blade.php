86001a9b21fc4f80d64655b8493c588e542ef9ea
Ryan Mitchell <ryan@thoughtcollective.com>
2020-09-04 07:34:57 +0100

    Connector --> Blade

---
+++ app/admin/formwidgets/connector/connector_items.blade.php
@@ -0,0 +1,6 @@
+@foreach ($fieldItems as $fieldItem)
+    {{ $this->makePartial('connector/connector_item', [
+        'item' => $fieldItem,
+        'index' => $loop->index,
+    ]) }}
+@endforeach
