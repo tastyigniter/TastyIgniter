55913d92b476a482a66298869ab37a737bf0c972
Sam Poyigi <sam@sampoyigi.com>
2020-12-05 14:08:48 +0000

    Deprecate Location_tables_model instead

---
+++ app/admin/models/Location_tables_model.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Admin\Models;
+
+use Model;
+
+/**
+ * Location tables Model Class
+ * @deprecated remove before v4. Added for backward compatibility, see Locationable
+ */
+class Location_tables_model extends Model
+{
+    /**
+     * @var string The database table name
+     */
+    protected $table = 'location_tables';
+
+    protected $primaryKey = 'table_id';
+
+    public $incrementing = FALSE;
+
+    public $casts = [
+        'location_id' => 'integer',
+        'table_id' => 'integer',
+    ];
+
+    public $relation = [
+        'belongsTo' => [
+            'tables' => ['Admin\Models\Tables_model'],
+        ],
+    ];
+}
