8e1f90c96be7eba78ffec6da5761cdc057107a1e
Sam Poyigi <sam@sampoyigi.com>
2020-12-05 14:09:35 +0000

    Make is_joinable enabled by default

--- app/admin/database/migrations/2020_08_16_000300_modify_columns_on_tables_reservations_table.php
+++ app/admin/database/migrations/2020_08_16_000300_modify_columns_on_tables_reservations_table.php
@@ -2,7 +2,6 @@
 
 namespace Admin\Database\Migrations;
 
-use Admin\Models\Tables_model;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
@@ -16,9 +15,11 @@ class ModifyColumnsOnTablesReservationsTable extends Migration
 
         Schema::table('tables', function (Blueprint $table) {
             $table->integer('extra_capacity')->default(0);
-            $table->boolean('is_joinable')->default(0);
+            $table->boolean('is_joinable')->default(1);
             $table->integer('priority')->default(0);
         });
+
+        Schema::dropIfExists('location_tables');
     }
 
     public function down()
@@ -35,11 +36,9 @@ class ModifyColumnsOnTablesReservationsTable extends Migration
         DB::table('location_tables')->get()->each(function ($model) {
             DB::table('locationables')->insert([
                 'location_id' => $model->location_id,
-                'locationable_type' => Tables_model::make()->getMorphClass(),
+                'locationable_type' => 'tables',
                 'locationable_id' => $model->table_id,
             ]);
         });
-
-        Schema::dropIfExists('location_tables');
     }
 }
