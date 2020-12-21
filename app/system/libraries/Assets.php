63546b6314e0fb11ed4fb3ae250f27690e3bf532
Sam Poyigi <sam@sampoyigi.com>
2020-12-21 23:53:19 +0000

    New Assets::mergeJsVars method

--- app/system/libraries/Assets.php
+++ app/system/libraries/Assets.php
@@ -51,7 +51,7 @@ class Assets
     /**
      * Set the default assets paths.
      *
-     * @param  string $path
+     * @param string $path
      *
      * @return void
      */
@@ -204,6 +204,15 @@ class Assets
         $this->assets['jsVars'] = array_merge($this->assets['jsVars'], $variables);
     }
 
+    public function mergeJsVars($key, $value)
+    {
+        $vars = array_get($this->assets['jsVars'], $key, []);
+
+        $value = array_merge($vars, $value);
+
+        array_set($this->assets['jsVars'], $key, $value);
+    }
+
     public function flush()
     {
         $this->assets = ['icon' => [], 'meta' => [], 'js' => [], 'css' => [], 'jsVars' => []];
@@ -314,17 +323,17 @@ class Assets
 
         if ($type == 'js') {
             $html = '<script'.Html::attributes(array_merge([
-                'charset' => strtolower(setting('charset', 'UTF-8')),
-                'type' => 'text/javascript',
-                'src' => asset($file),
-            ], $attributes)).'></script>'.PHP_EOL;
+                    'charset' => strtolower(setting('charset', 'UTF-8')),
+                    'type' => 'text/javascript',
+                    'src' => asset($file),
+                ], $attributes)).'></script>'.PHP_EOL;
         }
         else {
             $html = '<link'.Html::attributes(array_merge([
-                'rel' => 'stylesheet',
-                'type' => 'text/css',
-                'href' => asset($file),
-            ], $attributes)).'>'.PHP_EOL;
+                    'rel' => 'stylesheet',
+                    'type' => 'text/css',
+                    'href' => asset($file),
+                ], $attributes)).'>'.PHP_EOL;
         }
 
         return $html;
