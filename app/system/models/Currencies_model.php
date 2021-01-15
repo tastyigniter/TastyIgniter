6543d6c2c29f4882abb60b6c36f6532e0df9841c
Ryan Mitchell <ryan@thoughtcollective.com>
2021-01-15 08:01:23 +0000

    Add scopes for API usage

--- app/system/models/Currencies_model.php
+++ app/system/models/Currencies_model.php
@@ -47,6 +47,52 @@ class Currencies_model extends Currency
      */
     protected static $defaultCurrency;
 
+    public static $allowedSortingColumns = ['currency_name asc', 'currency_name desc', 'currency_code asc', 'currency_code desc'];
+
+    public function scopeListFrontEnd($query, $options = [])
+    {
+        extract(array_merge([
+            'page' => 1,
+            'pageLimit' => 20,
+            'enabled' => TRUE,
+            'sort' => 'currency_name asc',
+            'search' => '',
+        ], $options));
+
+        $searchableFields = ['currency_name', 'currency_code'];
+
+        if (!is_array($sort)) {
+            $sort = [$sort];
+        }
+
+        foreach ($sort as $_sort) {
+            if (in_array($_sort, self::$allowedSortingColumns)) {
+                $parts = explode(' ', $_sort);
+                if (count($parts) < 2) {
+                    $parts[] = 'desc';
+                }
+                [$sortField, $sortDirection] = $parts;
+                $query->orderBy($sortField, $sortDirection);
+            }
+        }
+
+        $search = trim($search);
+        if (strlen($search)) {
+            $query->search($search, $searchableFields);
+        }
+
+        if ($enabled) {
+            $query->isEnabled();
+        }
+
+        return $query->paginate($pageLimit, $page);
+    }
+
+    public function scopeIsEnabled($query)
+    {
+        return $query->where('currency_status', 1);
+    }
+
     public function makeDefault()
     {
         if (!$this->currency_status) {
