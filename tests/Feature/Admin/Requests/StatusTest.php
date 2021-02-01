b354adb1a405d96e2da7425877c1139025855c39
Ryan Mitchell <ryan@thoughtcollective.com>
2021-02-01 15:06:41 +0000

    Correct data names

--- tests/Feature/Admin/Requests/StatusTest.php
+++ tests/Feature/Admin/Requests/StatusTest.php
@@ -26,86 +26,88 @@ class StatusTest extends TestCase
             'request_should_fail_when_no_name_is_provided' => [
                 'passed' => FALSE,
                 'data' => [
-                    'for' => 'order',
+                    'status_for' => 'order',
                 ],
             ],
             'request_should_fail_when_name_has_less_than_2_characters' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => 'a',
+                    'status_name' => 'a',
                 ],
             ],
             'request_should_fail_when_name_has_more_than_32_characters' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->text(33),
+                    'status_name' => $faker->text(33),
                 ],
             ],
             'request_should_fail_when_for_is_not_provided' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->text(7),
-                    'notify' => true,
+                    'status_name' => $faker->text(7),
+                    'notify_customer' => true,
                 ],
             ],
             'request_should_fail_when_for_is_not_alphanumeric' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->text(7),
-                    'for' => $faker->randomElements(['!', '#', '$', '%', '~', '.', ':', ':'], 4),
+                    'status_name' => $faker->text(7),
+                    'status_for' => $faker->randomElements(['!', '#', '$', '%', '~', '.', ':', ':'], 4),
                 ],
             ],
             'request_should_fail_when_color_has_less_than_7_characters' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->text(7),
-                    'for' => 'order',
-                    'color' => $faker->text(6),
+                    'status_name' => $faker->text(7),
+                    'status_for' => 'order',
+                    'status_color' => $faker->text(6),
+                    'notify_customer' => true,
                 ],
             ],
             'request_should_fail_when_color_has_more_than_7_characters' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->text(7),
-                    'for' => 'order',
-                    'color' => $faker->text(8),
+                    'status_name' => $faker->text(7),
+                    'status_for' => 'order',
+                    'status_color' => $faker->text(8),
+                    'notify_customer' => true,
                 ],
             ],
             'request_should_fail_when_comment_has_more_than_1028_characters' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->text(7),
-                    'for' => 'order',
-                    'color' => $faker->text(7),
-                    'description' => $faker->text(1029),
+                    'status_name' => $faker->text(7),
+                    'status_for' => 'order',
+                    'status_color' => $faker->text(7),
+                    'status_comment' => $faker->text(1029),
                 ],
             ],
             'request_should_fail_when_notify_is_not_provided' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->text(7),
-                    'for' => 'order',
-                    'color' => $faker->text(7),
+                    'status_name' => $faker->text(7),
+                    'status_for' => 'order',
+                    'status_color' => $faker->text(7),
                 ],
             ],
             'request_should_fail_when_notify_is_not_boolean' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->text(7),
-                    'for' => 'order',
-                    'color' => $faker->text(7),
-                    'notify' => $faker->word(),
+                    'status_name' => $faker->text(7),
+                    'status_for' => 'order',
+                    'status_color' => $faker->text(7),
+                    'notify_customer' => $faker->word(),
                 ],
             ],
 
             'request_should_pass_when_data_is_provided' => [
                 'passed' => TRUE,
                 'data' => [
-                    'name' => $faker->text(7),
-                    'for' => 'order',
-                    'color' => $faker->text(7),
-                    'description' => $faker->sentence(),
-                    'status' => $faker->boolean(),
+                    'status_name' => $faker->text(7),
+                    'status_for' => 'order',
+                    'status_color' => $faker->text(7),
+                    'status_comment' => $faker->sentence(),
+                    'notify_customer' => $faker->boolean(),
                 ],
             ],
         ];
