4b8629d15da266750a541f14fc3154c92e721565
Ryan Mitchell <ryan@thoughtcollective.com>
2021-02-01 14:59:15 +0000

    Fix failures

--- tests/Feature/Admin/Requests/StatusTest.php
+++ tests/Feature/Admin/Requests/StatusTest.php
@@ -26,73 +26,74 @@ class StatusTest extends TestCase
             'request_should_fail_when_no_name_is_provided' => [
                 'passed' => FALSE,
                 'data' => [
-                    'for' => $faker->sentence(),
+                    'for' => 'order',
                 ],
             ],
             'request_should_fail_when_name_has_less_than_2_characters' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->sentence(1),
+                    'name' => $faker->text(1),
                 ],
             ],
             'request_should_fail_when_name_has_more_than_32_characters' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->sentence(33),
+                    'name' => $faker->text(33),
                 ],
             ],
             'request_should_fail_when_for_is_not_provided' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->word(),
+                    'name' => $faker->text(7),
                     'notify' => true,
                 ],
             ],
             'request_should_fail_when_for_is_not_alphanumeric' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->word(),
+                    'name' => $faker->text(7),
                     'for' => $faker->randomElements(['!', '#', '$', '%', '~', '.', ':', ':'], 4),
                 ],
             ],
             'request_should_fail_when_color_has_less_than_7_characters' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->word(),
+                    'name' => $faker->text(7),
                     'for' => 'order',
-                    'color' => $faker->sentence(6),
+                    'color' => $faker->text(6),
                 ],
             ],
             'request_should_fail_when_color_has_more_than_7_characters' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->word(),
+                    'name' => $faker->text(7),
                     'for' => 'order',
-                    'color' => $faker->sentence(8),
+                    'color' => $faker->text(8),
                 ],
             ],
             'request_should_fail_when_comment_has_more_than_1028_characters' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->sentence(1029),
+                    'name' => $faker->text(7),
                     'for' => 'order',
-                    'color' => $faker->sentence(7),
+                    'color' => $faker->text(7),
+                    'description' => $faker->text(1029),
                 ],
             ],
             'request_should_fail_when_notify_is_not_provided' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->word(),
+                    'name' => $faker->text(7),
                     'for' => 'order',
-                    'color' => $faker->sentence(7),
+                    'color' => $faker->text(7),
                 ],
             ],
             'request_should_fail_when_notify_is_not_boolean' => [
                 'passed' => FALSE,
                 'data' => [
-                    'name' => $faker->word(),
+                    'name' => $faker->text(7),
                     'for' => 'order',
-                    'color' => $faker->sentence(7),
+                    'color' => $faker->text(7),
                     'notify' => $faker->word(),
                 ],
             ],
@@ -100,9 +101,9 @@ class StatusTest extends TestCase
             'request_should_pass_when_data_is_provided' => [
                 'passed' => TRUE,
                 'data' => [
-                    'name' => $faker->word(),
+                    'name' => $faker->text(7),
                     'for' => 'order',
-                    'color' => $faker->word(7),
+                    'color' => $faker->text(7),
                     'description' => $faker->sentence(),
                     'status' => $faker->boolean(),
                 ],
