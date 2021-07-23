f61ae681da91317a29d77e6760d492943f8d4cc4
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2021-07-23 14:56:40 +0100

    Set default queue connection to sync

--- config/queue.php
+++ config/queue.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_CONNECTION', 'database'),
+    'default' => env('QUEUE_CONNECTION', 'sync'),
 
     /*
     |--------------------------------------------------------------------------
