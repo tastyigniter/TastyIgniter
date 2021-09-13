d313508b88e9086d9bee109b49c78d007be6b2dd
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2021-09-13 12:19:01 +0100

    wip

--- config/system.php
+++ config/system.php
@@ -197,6 +197,23 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Determines if assets combiner is enabled
+    |--------------------------------------------------------------------------
+    |
+    | This works by serialising a collection of asset paths and storing them
+    | in the session with a unique ID. The ID is then used by the system controller
+    | to generate a URL to the /_assets route.
+    |
+    | The unique ID is used to serve up the assets — minified, compiled, or both —
+    | when the combine route is accessed. To prevent the compilation and transmission
+    | of unmodified cached assets, special E-Tags are used.
+    |
+    */
+
+    'enableAssetCombiner' => TRUE,
+
+    /*
+    |--------------------------------------------------------------------------
     | Assets combiner URI
     |--------------------------------------------------------------------------
     |
@@ -204,7 +221,6 @@ return [
     |
     */
 
-    'enableAssetCombiner' => TRUE,
     'assetsCombinerUri' => '/_assets',
 
     /*
