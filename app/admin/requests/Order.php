c941bd8c652685c0e54a7d614598a24eaed51359
Ryan Mitchell <ryan@thoughtcollective.com>
2022-02-13 16:15:51 +0000

    Bring Order + Reservation requests over from API

--- app/admin/requests/Order.php
+++ app/admin/requests/Order.php
@@ -2,6 +2,7 @@
 
 namespace Admin\Requests;
 
+use Illuminate\Support\Facades\Request;
 use System\Classes\FormRequest;
 
 class Order extends FormRequest
@@ -11,8 +12,54 @@ class Order extends FormRequest
         return static::DATA_TYPE_POST;
     }
 
+    public function attributes()
+    {
+        return [
+            'first_name' => lang('igniter.cart::default.checkout.label_first_name'),
+            'last_name' => lang('igniter.cart::default.checkout.label_last_name'),
+            'email' => lang('igniter.cart::default.checkout.label_email'),
+            'telephone' => lang('igniter.cart::default.checkout.label_telephone'),
+            'comment' => lang('igniter.cart::default.checkout.label_comment'),
+            'payment' => lang('igniter.cart::default.checkout.label_payment_method'),
+            'order_type' => lang('igniter.cart::default.checkout.label_order_type'),
+            'address_id' => lang('igniter.cart::default.checkout.label_address'),
+            'address.address_1' => lang('igniter.cart::default.checkout.label_address_1'),
+            'address.address_2' => lang('igniter.cart::default.checkout.label_address_2'),
+            'address.city' => lang('igniter.cart::default.checkout.label_city'),
+            'address.state' => lang('igniter.cart::default.checkout.label_state'),
+            'address.postcode' => lang('igniter.cart::default.checkout.label_postcode'),
+            'address.country_id' => lang('igniter.cart::default.checkout.label_country'),
+        ];
+    }
+
     public function rules()
     {
-        return [];
+        $method = Request::method();
+
+        $rules = [
+            'first_name' => ['between:1,48'],
+            'last_name' => ['between:1,48'],
+            'email' => ['sometimes', 'required', 'email:filter', 'max:96'],
+            'comment' => ['max:500'],
+            'payment' => ['sometimes', 'required', 'alpha_dash'],
+        ];
+
+        if ($method == 'post') {
+            $rules['first_name'][] = 'required';
+            $rules['last_name'][] = 'required';
+            $rules['order_type'][] = 'required';
+        }
+
+        if (Request::input('order_type', 'collection') == 'delivery') {
+            $rules['address_id'] = ['required', 'integer'];
+            $rules['address.address_1'] = ['required', 'min:3', 'max:128'];
+            $rules['address.address_2'] = ['sometimes', 'min:3', 'max:128'];
+            $rules['address.city'] = ['sometimes', 'min:2', 'max:128'];
+            $rules['address.state'] = ['sometimes', 'max:128'];
+            $rules['address.postcode'] = ['string'];
+            $rules['address.country_id'] = ['sometimes', 'required', 'integer'];
+        }
+
+        return $rules;
     }
 }
--- app/admin/requests/Reservation.php
+++ app/admin/requests/Reservation.php
@@ -9,6 +9,7 @@ class Reservation extends FormRequest
     public function attributes()
     {
         return [
+            'table_id' => lang('admin::lang.reservations.column_table'),
             'location_id' => lang('admin::lang.reservations.text_restaurant'),
             'first_name' => lang('admin::lang.reservations.label_first_name'),
             'last_name' => lang('admin::lang.reservations.label_last_name'),
@@ -17,12 +18,14 @@ class Reservation extends FormRequest
             'reserve_date' => lang('admin::lang.reservations.label_reservation_date'),
             'reserve_time' => lang('admin::lang.reservations.label_reservation_time'),
             'guest_num' => lang('admin::lang.reservations.label_guest'),
+            'comment' => lang('igniter.reservation::default.label_comment'),
         ];
     }
 
     public function rules()
     {
         return [
+            'table_id' => ['integer'],
             'location_id' => ['sometimes', 'required', 'integer'],
             'first_name' => ['required', 'between:1,48'],
             'last_name' => ['required', 'between:1,48'],
@@ -31,6 +34,7 @@ class Reservation extends FormRequest
             'reserve_date' => ['required', 'valid_date'],
             'reserve_time' => ['required', 'valid_time'],
             'guest_num' => ['required', 'integer'],
+            'comment' => ['max:520'],
         ];
     }
 }
