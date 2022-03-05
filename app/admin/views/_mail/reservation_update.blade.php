4e016815c0eb74df1aa0b8dd084566c38ce421db
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2022-03-05 13:18:07 +0000

    Fix url issue in reservation mail template.
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- app/admin/views/_mail/reservation_update.blade.php
+++ app/admin/views/_mail/reservation_update.blade.php
@@ -12,12 +12,13 @@ To view your reservation progress, click the link below
 ==
 Hi {{ $first_name }} {{ $last_name }},
 
-Your reservation **{{ $reservation_number }}** at **{{ $location_name }}** has been updated to the following status: <br>
+Your reservation **{{ $reservation_number }}** at **{{ $location_name }}** has been updated to the following status:
+<br>
 **{{ $status_name }}**
 
 The comments for your reservation are: <br>
 {{ $status_comment }}
 
-@partial('button', ['url' => '{{ $reservation_view_url }}', 'type' => 'primary'])
+@partial('button', ['url' => $reservation_view_url, 'type' => 'primary'])
 View your reservation status
 @endpartial
