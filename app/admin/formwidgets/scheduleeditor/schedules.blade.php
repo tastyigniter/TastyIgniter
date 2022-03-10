992d4ce6444805c3132e3635a01b6fd222063554
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2022-03-10 22:11:52 +0000

    Minor fixes
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- app/admin/formwidgets/scheduleeditor/schedules.blade.php
+++ app/admin/formwidgets/scheduleeditor/schedules.blade.php
@@ -1,16 +1,16 @@
-<div class="row">
-    @foreach ($schedules as $schedule)
-        <div class="col-lg-4 py-3">
+<div class="d-flex flex-nowrap overflow-auto">
+    @foreach ($schedules as $scheduleCode => $schedule)
+        <div class="col-lg-3 {{ $loop->first ? 'py-2 pr-2 pl-0' : 'p-2' }}">
             <div
                 id="{{ $this->getId('item-'.$loop->iteration) }}"
                 class="card bg-light shadow-sm mb-0"
                 data-editor-control="load-schedule"
-                data-schedule-code="{{ $schedule->name }}"
+                data-schedule-code="{{ $scheduleCode }}"
                 role="button"
             >
                 <div class="card-body">
                     <div class="flex-fill">
-                        <h5 class="card-title">{{ lang('admin::lang.text_'.$schedule->name).' '.lang('admin::lang.locations.text_schedule') }}</h5>
+                        <h5 class="card-title">{{ lang($schedule->name).' '.lang('admin::lang.locations.text_schedule') }}</h5>
                         <p class="card-text">{{ lang('admin::lang.locations.text_'.$schedule->type) }}</p>
                     </div>
 
