16f24e059f30789ef1071d5ad9c1ef5b1f497fa3
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2022-06-06 13:33:51 +0100

    Add permissions to order, reservation delete bulk action #986
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- app/admin/models/config/reservations_model.php
+++ app/admin/models/config/reservations_model.php
@@ -59,6 +59,7 @@ $config['list']['bulkActions'] = [
         'label' => 'lang:admin::lang.button_delete',
         'class' => 'btn btn-light text-danger',
         'data-request-confirm' => 'lang:admin::lang.alert_warning_confirm',
+        'permissions' => 'Admin.DeleteOrders',
     ],
 ];
 
