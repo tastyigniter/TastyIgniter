000aaabf993e07374d0e6b84ae76e882c6f8b826
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2022-10-19 18:38:40 +0100

    Support multiple extension paths
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- app/admin/classes/BasePaymentGateway.php
+++ app/admin/classes/BasePaymentGateway.php
@@ -40,8 +40,8 @@ class BasePaymentGateway extends ModelAction
     {
         parent::__construct($model);
 
-        $calledClass = strtolower(get_called_class());
-        $this->configPath = extension_path(File::normalizePath($calledClass));
+        $reflector = new \ReflectionClass($calledClass = get_called_class());
+        $this->configPath = dirname($reflector->getFileName()).'/'.basename(File::normalizePath(strtolower($calledClass)));
 
         $formConfig = $this->loadConfig($this->defineFieldsConfig(), ['fields']);
         $this->configFields = array_get($formConfig, 'fields');
