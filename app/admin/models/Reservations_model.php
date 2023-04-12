ea6f83aba4fc6b46e2e38695f3ac70bdc6f845e5
Sam <6567634+sampoyigi@users.noreply.github.com>
2023-04-12 17:01:56 +0100

    Bring back option to manually set reservation tables
    Signed-off-by: Sam <6567634+sampoyigi@users.noreply.github.com>

--- app/admin/models/Reservations_model.php
+++ app/admin/models/Reservations_model.php
@@ -254,7 +254,7 @@ class Reservations_model extends Model
     public function getTableNameAttribute()
     {
         return ($this->tables && $this->tables->isNotEmpty())
-            ? implode(', ', $this->tables->pluck('name')->all())
+            ? implode(', ', $this->tables->pluck('table_name')->all())
             : '';
     }
 
