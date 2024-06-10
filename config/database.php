9b894bd82a6fc2a95e4c8e5af108585fe50b5481
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-06-10 13:45:18 +0100

    Bring back DB_PREFIX env
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
+            'prefix' => env('DB_PREFIX', ''),
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
