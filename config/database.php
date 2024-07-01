10e10580164cdbf5742848dbf171028620e0b7f4
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-07-01 15:32:45 +0100

    Disable DB strict mode
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
             'prefix' => env('DB_PREFIX', ''),
             'prefix_indexes' => true,
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
