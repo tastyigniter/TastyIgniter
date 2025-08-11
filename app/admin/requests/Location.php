081b91cd2d81255e7a1009f7068d3e9117cf9d13
Antonio Zhu <antonio@dineabyte.de>
2025-08-12 00:59:26 +0200

    Update Location.php (#1126)
    * Update HasLocationOptions.php
    
    Necessary for persisting new options in API calls
    
    * Update Location.php

--- app/admin/requests/Location.php
+++ app/admin/requests/Location.php
@@ -32,16 +32,16 @@ class Location extends FormRequest
     public function rules()
     {
         return [
-            'location_name' => ['required', 'between:2,32'],
-            'location_email' => ['required', 'email:filter', 'max:96'],
+            'location_name' => ['sometimes', 'required', 'between:2,32'],
+            'location_email' => ['sometimes', 'required', 'email:filter', 'max:96'],
             'location_telephone' => ['sometimes'],
-            'location_address_1' => ['required', 'between:2,128'],
+            'location_address_1' => ['sometimes', 'required', 'between:2,128'],
             'location_address_2' => ['max:128'],
             'location_city' => ['max:128'],
             'location_state' => ['max:128'],
             'location_postcode' => ['max:10'],
-            'location_country_id' => ['required', 'integer'],
-            'options.auto_lat_lng' => ['required', 'boolean'],
+            'location_country_id' => ['sometimes', 'required', 'integer'],
+            'options.auto_lat_lng' => ['sometimes', 'required', 'boolean'],
             'location_lat' => ['sometimes', 'numeric'],
             'location_lng' => ['sometimes', 'numeric'],
             'description' => ['max:3028'],
